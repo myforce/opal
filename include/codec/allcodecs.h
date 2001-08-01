50c2f069c2af0ed261dc78066d9759e2a9813151
Robert Jongbloed <robertj@voxlucida.com.au>
2001-08-01 05:03:26 +0000

    Changes to allow control of linking software transcoders, use macros   to force linking.
    git-svn-id: svn://svn.code.sf.net/p/opalvoip/code/opal/trunk@12547 023b2edf-31b2-4de3-b41e-bca80c47788f

---
+++ include/codec/allcodecs.h
@@ -0,0 +1,54 @@
+/*
+ * g711codec.h
+ *
+ * Open Phone Abstraction Library (OPAL)
+ * Formally known as the Open H323 project.
+ *
+ * Copyright (c) 2001 Equivalence Pty. Ltd.
+ *
+ * The contents of this file are subject to the Mozilla Public License
+ * Version 1.0 (the "License"); you may not use this file except in
+ * compliance with the License. You may obtain a copy of the License at
+ * http://www.mozilla.org/MPL/
+ *
+ * Software distributed under the License is distributed on an "AS IS"
+ * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See
+ * the License for the specific language governing rights and limitations
+ * under the License.
+ *
+ * The Original Code is Open Phone Abstraction Library.
+ *
+ * The Initial Developer of the Original Code is Equivalence Pty. Ltd.
+ *
+ * Contributor(s): ______________________________________.
+ *
+ * $Log: allcodecs.h,v $
+ * Revision 1.2002  2001/08/01 05:03:26  robertj
+ * Changes to allow control of linking software transcoders, use macros
+ *   to force linking.
+ *
+ */
+
+#ifndef __CODEC_ALLCODECS_H
+#define __CODEC_ALLCODECS_H
+
+#include <codec/g711codec.h>
+OPAL_REGISTER_G711();
+
+#include <codec/gsmcodec.h>
+OPAL_REGISTER_GSM0610();
+
+#include <codec/mscodecs.h>
+OPAL_REGISTER_MSCODECS();
+
+#include <codec/lpc10codec.h>
+OPAL_REGISTER_LPC10();
+
+#include <codec/h261codec.h>
+OPAL_REGISTER_H261();
+
+
+#endif // __CODEC_ALLCODECS_H
+
+
+/////////////////////////////////////////////////////////////////////////////
