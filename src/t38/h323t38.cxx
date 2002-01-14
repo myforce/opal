a0ecfc65c67c5983c3d8fc0b0581119e1e91deed
Robert Jongbloed <robertj@voxlucida.com.au>
2002-01-14 06:35:59 +0000

    Updated to OpenH323 v1.7.9
    git-svn-id: svn://svn.code.sf.net/p/opalvoip/code/opal/trunk@12736 023b2edf-31b2-4de3-b41e-bca80c47788f

--- src/t38/h323t38.cxx
+++ src/t38/h323t38.cxx
@@ -24,7 +24,10 @@
  * Contributor(s): ______________________________________.
  *
  * $Log: h323t38.cxx,v $
- * Revision 1.2006  2001/11/12 05:32:12  robertj
+ * Revision 1.2007  2002/01/14 06:35:59  robertj
+ * Updated to OpenH323 v1.7.9
+ *
+ * Revision 2.5  2001/11/12 05:32:12  robertj
  * Added OpalTransportAddress::GetIpAddress when don't need port number.
  *
  * Revision 2.4  2001/10/05 00:22:14  robertj
@@ -41,6 +44,34 @@
  *
  * Revision 2.0  2001/07/27 15:48:25  robertj
  * Conversion of OpenH323 to Open Phone Abstraction Library (OPAL)
+ *
+ * Revision 1.14  2002/01/10 04:08:42  robertj
+ * Fixed missing bit rate in mode request PDU.
+ *
+ * Revision 1.13  2002/01/09 00:21:40  robertj
+ * Changes to support outgoing H.245 RequstModeChange.
+ *
+ * Revision 1.12  2002/01/01 23:27:50  craigs
+ * Added CleanupOnTermination functions
+ * Thanks to Vyacheslav Frolov
+ *
+ * Revision 1.11  2001/12/22 03:21:58  robertj
+ * Added create protocol function to H323Connection.
+ *
+ * Revision 1.10  2001/12/22 01:55:06  robertj
+ * Removed vast quatities of redundent code that is done by ancestor class.
+ *
+ * Revision 1.9  2001/12/19 09:15:43  craigs
+ * Added changes from Vyacheslav Frolov
+ *
+ * Revision 1.8  2001/12/14 08:36:36  robertj
+ * More implementation of T.38, thanks Adam Lazur
+ *
+ * Revision 1.7  2001/11/20 03:04:30  robertj
+ * Added ability to reuse t38 channels with same session ID.
+ *
+ * Revision 1.6  2001/11/09 05:39:54  craigs
+ * Added initial T.38 support thanks to Adam Lazur
  *
  * Revision 1.5  2001/09/12 07:48:05  robertj
  * Fixed various problems with tracing.
@@ -75,6 +106,9 @@
 
 
 #define new PNEW
+
+#define FAX_BIT_RATE 144  //14.4k
+
 
 
 /////////////////////////////////////////////////////////////////////////////
@@ -119,43 +153,74 @@ unsigned H323_T38Capability::GetSubType() const
 
 H323Channel * H323_T38Capability::CreateChannel(H323Connection & connection,
                                                 H323Channel::Directions direction,
-                                                unsigned,
-                               const H245_H2250LogicalChannelParameters *) const
+                                                unsigned int sessionID,
+                             const H245_H2250LogicalChannelParameters * /*params*/) const
 {
-  return new H323_T38Channel(connection, *this, direction);
+  PTRACE(1, "H323T38\tCreateChannel, sessionID=" << sessionID << " direction=" << direction);
+
+  H323_T38Channel * chan = (H323_T38Channel *)connection.FindChannel(sessionID,
+                                                direction == H323Channel::IsTransmitter);
+  return new H323_T38Channel(connection, *this, direction,
+                             chan != NULL ? chan->GetHandler() : NULL);
 }
 
 
-BOOL H323_T38Capability::OnSendingPDU(H245_DataApplicationCapability & cap) const
+BOOL H323_T38Capability::OnSendingPDU(H245_DataApplicationCapability & pdu) const
 {
-  cap.m_maxBitRate = 144; // 14.4kbps
-  cap.m_application.SetTag(H245_DataApplicationCapability_application::e_t38fax);
-  H245_DataApplicationCapability_application_t38fax & fax = cap.m_application;
-    H245_DataProtocolCapability & proto = fax.m_t38FaxProtocol;
-  H245_T38FaxProfile & profile = fax.m_t38FaxProfile;
+  PTRACE(3, "H323T38\tOnSendingPDU for capability");
+
+  pdu.m_maxBitRate = FAX_BIT_RATE;
+  pdu.m_application.SetTag(H245_DataApplicationCapability_application::e_t38fax);
+  H245_DataApplicationCapability_application_t38fax & fax = pdu.m_application;
+  return OnSendingPDU(fax.m_t38FaxProtocol, fax.m_t38FaxProfile);
+}
+
+
+BOOL H323_T38Capability::OnSendingPDU(H245_DataMode & pdu) const
+{
+  pdu.m_bitRate = FAX_BIT_RATE;
+  pdu.m_application.SetTag(H245_DataMode_application::e_t38fax);
+  H245_DataMode_application_t38fax & fax = pdu.m_application;
+  return OnSendingPDU(fax.m_t38FaxProtocol, fax.m_t38FaxProfile);
+}
+
+
+BOOL H323_T38Capability::OnSendingPDU(H245_DataProtocolCapability & proto,
+                                      H245_T38FaxProfile & profile) const
+{
   if (mode == e_UDP) {
     proto.SetTag(H245_DataProtocolCapability::e_udp);
+    profile.m_t38FaxRateManagement.SetTag(H245_T38FaxRateManagement::e_transferredTCF); // recommended for UDP
+
     profile.IncludeOptionalField(H245_T38FaxProfile::e_t38FaxUdpOptions);
+    profile.m_t38FaxUdpOptions.IncludeOptionalField(H245_T38FaxUdpOptions::e_t38FaxMaxBuffer);
     profile.m_t38FaxUdpOptions.m_t38FaxMaxBuffer = 200;
+    profile.m_t38FaxUdpOptions.IncludeOptionalField(H245_T38FaxUdpOptions::e_t38FaxMaxDatagram);
     profile.m_t38FaxUdpOptions.m_t38FaxMaxDatagram = 72;
     profile.m_t38FaxUdpOptions.m_t38FaxUdpEC.SetTag(H245_T38FaxUdpOptions_t38FaxUdpEC::e_t38UDPRedundancy);
   }
   else {
     proto.SetTag(H245_DataProtocolCapability::e_tcp);
+    profile.m_t38FaxRateManagement.SetTag(H245_T38FaxRateManagement::e_localTCF); // recommended for TCP
+
     profile.IncludeOptionalField(H245_T38FaxProfile::e_t38FaxTcpOptions);
     profile.m_t38FaxTcpOptions.m_t38TCPBidirectionalMode = mode == e_SingleTCP;
   }
+
   return TRUE;
 }
 
 
 BOOL H323_T38Capability::OnReceivedPDU(const H245_DataApplicationCapability & cap)
 {
+  PTRACE(3, "H323T38\tOnRecievedPDU for capability");
+
   if (cap.m_application.GetTag() != H245_DataApplicationCapability_application::e_t38fax)
     return FALSE;
 
   const H245_DataApplicationCapability_application_t38fax & fax = cap.m_application;
   const H245_DataProtocolCapability & proto = fax.m_t38FaxProtocol;
+
   if (proto.GetTag() == H245_DataProtocolCapability::e_udp)
     mode = e_UDP;
   else {
@@ -170,15 +235,67 @@ BOOL H323_T38Capability::OnReceivedPDU(const H245_DataApplicationCapability & ca
 }
 
 
-/////////////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////
 
-H323_T38Channel::H323_T38Channel(H323Connection & conn,
-                                   const H323_T38Capability & cap,
-                                   Directions dir)
-  : H323DataChannel(conn, cap, dir)
+H323_T38Channel::H323_T38Channel(H323Connection & connection,
+                        const H323_T38Capability & capability,
+                        H323Channel::Directions dir,
+                        OpalT38Protocol * handler)
+        : H323DataChannel(connection, capability, dir)
 {
-  separateReverseChannel = cap.GetTransportMode() != H323_T38Capability::e_SingleTCP;
-  usesTCP = cap.GetTransportMode() != H323_T38Capability::e_UDP;
+  PTRACE(3, "H323T38\tH323 channel created");
+
+  separateReverseChannel = capability.GetTransportMode() != H323_T38Capability::e_SingleTCP;
+  usesTCP = capability.GetTransportMode() != H323_T38Capability::e_UDP;
+
+  if (handler != NULL) {
+    PTRACE(3, "H323T38\tConnected to existing T.38 handler");
+    t38handler = handler;
+  }
+  else {
+    PTRACE(3, "H323T38\tCreating new T.38 handler");
+    t38handler = connection.CreateT38ProtocolHandler();
+  }
+}
+
+
+H323_T38Channel::~H323_T38Channel()
+{
+}
+
+void H323_T38Channel::Close()
+{
+  if (terminating)
+    return;
+
+  PTRACE(3, "H323T38\tCleanUpOnTermination");
+    
+  if (t38handler != NULL) 
+    t38handler->Close();
+
+  H323DataChannel::Close();
+}
+
+
+BOOL H323_T38Channel::OnSendingPDU(H245_OpenLogicalChannel & open) const
+{
+  if (t38handler != NULL)
+    return H323DataChannel::OnSendingPDU(open);
+
+  PTRACE(1, "H323T38\tNo protocol handler, aborting OpenLogicalChannel.");
+  return FALSE;
+}
+
+
+BOOL H323_T38Channel::OnReceivedPDU(const H245_OpenLogicalChannel & open,
+                                    unsigned & errorCode)
+{
+  if (t38handler != NULL)
+    return H323DataChannel::OnReceivedPDU(open, errorCode);
+
+  errorCode = H245_OpenLogicalChannelReject_cause::e_unspecified;
+  PTRACE(1, "H323T38\tNo protocol handler, refusing OpenLogicalChannel.");
+  return FALSE;
 }
 
 
@@ -208,35 +325,22 @@ void H323_T38Channel::Receive()
 
 void H323_T38Channel::Transmit()
 {
-  PTRACE(2, "H323T38\tTransmit thread started.");
+  if (terminating)
+    return;
 
-  if (t38handler != NULL) {
-    if (transport != NULL)
-      t38handler->Originate(*transport);
-    else {
-      PTRACE(1, "H323T38\tNo transport, aborting thread.");
-    }
-  }
+  PTRACE(2, "H323T38\tTransmit thread starting");
+
+  if (t38handler != NULL)
+    t38handler->Originate(*transport);
   else {
-    PTRACE(1, "H323T38\tNo protocol handler, aborting thread.");
+    PTRACE(1, "H323T38\tTransmit no proto handler");
   }
 
   connection.CloseLogicalChannelNumber(number);
 
-  PTRACE(2, "H323T38\tTransmit thread ended");
+  PTRACE(2, "H323T38\tTransmit thread terminating");
 }
 
-
-BOOL H323_T38Channel::CreateListener()
-{
-  if (listener != NULL)
-    return TRUE;
-
-  if (usesTCP)
-    return H323DataChannel::CreateListener();
-
-  return CreateTransport();
-}
 
 BOOL H323_T38Channel::CreateTransport()
 {
@@ -248,12 +352,27 @@ BOOL H323_T38Channel::CreateTransport()
 
   PIPSocket::Address ip;
   if (!connection.GetControlChannel().GetLocalAddress().GetIpAddress(ip)) {
-    PTRACE(2, "H323T38\tTrying to use UDP when base transport is not TCP/IP");
+    PTRACE(2, "H323T38\tTrying to use UDP when base transport is not IP");
     PIPSocket::GetHostAddress(ip);
   }
 
   transport = new OpalTransportUDP(connection.GetEndPoint(), ip);
+  PTRACE(3, "H323T38\tCreated transport: " << *transport);
   return TRUE;
+}
+
+
+BOOL H323_T38Channel::CreateListener()
+{
+  if (listener != NULL)
+    return TRUE;
+
+  if (usesTCP) {
+    return H323DataChannel::CreateListener();
+    PTRACE(3, "H323T38\tCreated listener " << *listener);
+  }
+
+  return CreateTransport();
 }
 
 
