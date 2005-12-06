8ee41d71d6c4550bbdb861d7eafa37ede451c16a
Craig Southeren <craigs@postincrement.com>
2005-12-06 06:34:10 +0000

    Added configure support for Sangoma and empty LID source and header files

---
+++ src/lids/sangomalid.cxx
@@ -0,0 +1,40 @@
+/*
+ * sangomalid.cxx
+ *
+ * Sangoma Line Interface Device
+ *
+ * Open Phone Abstraction Library
+ *
+ * Copyright (C) 2005 Post Increment
+ *
+ * The contents of this file are subject to the Mozilla Public License
+ * Version 1.0 (the "License"); you may not use this file except in
+ * compliance with the License. You may obtain a copy of the License at
+ * http://www.mozilla.org/MPL/
+ *
+ * Software distributed under the License is distributed on an "AS IS"
+ * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See
+ * the License for the specific language governing rights and limitations
+ * under the License.
+ *
+ * The Original Code is Open Phone Abstraction Library
+ *
+ * The Initial Developer of the Original Code is Post Increment
+ *
+ * Contributor(s): ______________________________________.
+ *
+ * $Log: sangomalid.cxx,v $
+ * Revision 1.1  2005/12/06 06:34:10  csoutheren
+ * Added configure support for Sangoma and empty LID source and header files
+ *
+ */
+
+#define __LINUX__
+#define _LINUX_IF_H
+
+#include "/usr/local/include/libsangoma.h"
+
+#include <lids/sangomalid.h>
+
+
+
