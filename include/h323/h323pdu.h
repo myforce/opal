79c5c113e81e17a1fb3d48ceb2cf1d90411e3d11
Craig Southeren <craigs@postincrement.com>
2006-06-20 05:21:39 +0000

    Update to ASN for H.225v6
    git-svn-id: svn://svn.code.sf.net/p/opalvoip/code/opal/trunk@15764 023b2edf-31b2-4de3-b41e-bca80c47788f

--- include/h323/h323pdu.h
+++ include/h323/h323pdu.h
@@ -27,7 +27,10 @@
  * Contributor(s): ______________________________________.
  *
  * $Log: h323pdu.h,v $
- * Revision 1.2015  2005/11/30 13:35:26  csoutheren
+ * Revision 1.2016  2006/06/20 05:21:39  csoutheren
+ * Update to ASN for H.225v6
+ *
+ * Revision 2.14  2005/11/30 13:35:26  csoutheren
  * Changed tags for Doxygen
  *
  * Revision 2.13  2005/09/04 06:23:38  rjongbloed
@@ -315,7 +318,7 @@ class H225_RAS;
 class OpalGloballyUniqueID;
 
 
-#define H225_PROTOCOL_VERSION 4
+#define H225_PROTOCOL_VERSION 6
 #define H245_PROTOCOL_VERSION 11
 
 
