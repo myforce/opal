3252bffd4547048c162d55faaa157d43ffe9d652
Robert Jongbloed <robertj@voxlucida.com.au>
2006-11-25 03:44:02 +0000

    Changed CAPI library interface so uses late binding on Win32 DLL preventing annoying   dialogs from appearing of no ISDN on your system.

--- plugins/LID/CAPI/Win32/capi2032.h
+++ plugins/LID/CAPI/Win32/capi2032.h
@@ -2,8 +2,8 @@
  * capi2032.h
  */
 
-# ifndef __CAPI20_H
-# define __CAPI20_H
+# ifndef __CAPI2032_H
+# define __CAPI2032_H
 
 # ifdef __cplusplus
 extern "C" {
@@ -11,36 +11,104 @@ extern "C" {
 
 #include <windows.h>
 
-DWORD APIENTRY CAPI_REGISTER(DWORD MessageBufferSize,
+// Function prototypes for early or late binding on DLL
+
+#define CAPI_DLL_NAME "CAPI2032.DLL"
+
+
+
+#define CAPI_REGISTER_ORDINAL 1
+
+typedef DWORD (WINAPI *CAPI_REGISTER_FUNCTION)(DWORD MessageBufferSize,
+                                                 DWORD MaxLogicalConnection,
+                                                 DWORD MaxBDataBlocks,
+                                                 DWORD MaxBDataLen,
+                                                 DWORD *pApplID);
+
+DWORD WINAPI CAPI_REGISTER(DWORD MessageBufferSize,
                              DWORD MaxLogicalConnection,
                              DWORD MaxBDataBlocks,
                              DWORD MaxBDataLen,
                              DWORD *pApplID);
 
-DWORD APIENTRY CAPI_RELEASE(DWORD ApplID);
 
-DWORD APIENTRY CAPI_PUT_MESSAGE(DWORD ApplID, PVOID pCAPIMessage);
 
-DWORD APIENTRY CAPI_GET_MESSAGE(DWORD ApplID, PVOID *ppCAPIMessage);
+#define CAPI_RELEASE_ORDINAL 2
 
-DWORD APIENTRY CAPI_WAIT_FOR_SIGNAL(DWORD ApplID);
+typedef DWORD (WINAPI *CAPI_RELEASE_FUNCTION)(DWORD ApplID);
 
-VOID  APIENTRY CAPI_GET_MANUFACTURER(CHAR *szBuffer);
+DWORD WINAPI CAPI_RELEASE(DWORD ApplID);
 
-DWORD APIENTRY CAPI_GET_VERSION(DWORD *pCAPIMajor,
+
+
+#define CAPI_PUT_MESSAGE_ORDINAL 3
+
+typedef DWORD (WINAPI *CAPI_PUT_MESSAGE_FUNCTION)(DWORD ApplID, PVOID pCAPIMessage);
+
+DWORD WINAPI CAPI_PUT_MESSAGE(DWORD ApplID, PVOID pCAPIMessage);
+
+
+
+#define CAPI_GET_MESSAGE_ORDINAL 4
+
+typedef DWORD (WINAPI *CAPI_GET_MESSAGE_FUNCTION)(DWORD ApplID, PVOID *ppCAPIMessage);
+
+DWORD WINAPI CAPI_GET_MESSAGE(DWORD ApplID, PVOID *ppCAPIMessage);
+
+
+
+#define CAPI_WAIT_FOR_SIGNAL_ORDINAL 5
+
+typedef DWORD (WINAPI *CAPI_WAIT_FOR_SIGNAL_FUNCTION)(DWORD ApplID);
+
+DWORD WINAPI CAPI_WAIT_FOR_SIGNAL(DWORD ApplID);
+
+
+
+#define CAPI_GET_MANUFACTURER_ORDINAL 6
+
+typedef DWORD (WINAPI *CAPI_GET_MANUFACTURER_FUNCTION)(char *szBuffer);
+
+DWORD WINAPI CAPI_GET_MANUFACTURER(char *szBuffer);
+
+
+
+#define CAPI_GET_VERSION_ORDINAL 7
+
+typedef DWORD (WINAPI *CAPI_GET_VERSION_FUNCTION)(DWORD *pCAPIMajor,
+                                                    DWORD *pCAPIMinor,
+                                                    DWORD *pManufacturerMajor,
+                                                    DWORD *pManufacturerMinor);
+
+DWORD WINAPI CAPI_GET_VERSION(DWORD *pCAPIMajor,
                                 DWORD *pCAPIMinor,
                                 DWORD *pManufacturerMajor,
                                 DWORD *pManufacturerMinor);
 
-DWORD APIENTRY CAPI_GET_SERIAL_NUMBER(CHAR *szBuffer);
 
-DWORD APIENTRY CAPI_GET_PROFILE(PVOID pBuffer, DWORD CtrlNr);
 
-DWORD APIENTRY CAPI_INSTALLED(VOID);
+#define CAPI_GET_SERIAL_NUMBER_ORDINAL 8
 
-DWORD APIENTRY CAPI_MANUFACTURER(DWORD Function, DWORD Qualifier,
-				 CHAR *InData,   DWORD sizeInData,
-			  CHAR *OutData,  DWORD sizeOutData);
+typedef DWORD (WINAPI *CAPI_GET_SERIAL_NUMBER_FUNCTION)(char *szBuffer);
+
+DWORD WINAPI CAPI_GET_SERIAL_NUMBER(char *szBuffer);
+
+
+
+#define CAPI_GET_PROFILE_ORDINAL 9
+
+typedef DWORD (WINAPI *CAPI_GET_PROFILE_FUNCTION)(PVOID pBuffer, DWORD CtrlNr);
+
+DWORD WINAPI CAPI_GET_PROFILE(PVOID pBuffer, DWORD CtrlNr);
+
+
+
+#define CAPI_INSTALLED_ORDINAL 10
+
+typedef DWORD (WINAPI *CAPI_INSTALLED_FUNCTION)(VOID);
+
+DWORD WINAPI CAPI_INSTALLED(VOID);
+
 
 
 #ifdef __cplusplus
@@ -51,5 +119,5 @@ DWORD APIENTRY CAPI_MANUFACTURER(DWORD Function, DWORD Qualifier,
 #include "capiutils.h"
 #endif
 
-#endif /* __CAPI20_H */
+#endif /* __CAPI2032_H */
 
