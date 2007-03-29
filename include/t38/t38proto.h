5665e98e9be85395f28673f110910c474091a2df
Craig Southeren <craigs@postincrement.com>
2007-03-29 05:19:54 +0000

    Implement T.38 and fax
    git-svn-id: svn://svn.code.sf.net/p/opalvoip/code/opal/trunk@17366 023b2edf-31b2-4de3-b41e-bca80c47788f

--- include/t38/t38proto.h
+++ include/t38/t38proto.h
@@ -24,7 +24,10 @@
  * Contributor(s): ______________________________________.
  *
  * $Log: t38proto.h,v $
- * Revision 1.2009  2005/02/21 12:19:48  rjongbloed
+ * Revision 1.2010  2007/03/29 05:19:54  csoutheren
+ * Implement T.38 and fax
+ *
+ * Revision 2.8  2005/02/21 12:19:48  rjongbloed
  * Added new "options list" to the OpalMediaFormat class.
  *
  * Revision 2.7  2003/01/07 04:39:53  robertj
@@ -94,16 +97,19 @@
 #endif
 
 
-#include <opal/mediafmt.h>
+#include <ptlib/pipechan.h>
 
+#include <opal/mediafmt.h>
+#include <opal/mediastrm.h>
+#include <opal/endpoint.h>
 
 class OpalTransport;
 class T38_IFPPacket;
 class PASN_OctetString;
 
-
-#define OPAL_T38 "T.38"
-
+namespace PWLibStupidLinkerHacks {
+  extern int t38Loader;
+};
 
 ///////////////////////////////////////////////////////////////////////////////
 
@@ -249,7 +255,429 @@ class OpalT38Protocol : public PObject
 };
 
 
+///////////////////////////////////////////////////////////////////////////////
+
+/**
+  *  This format is identical to the OpalPCM16 except that it uses a different
+  *  sessionID in order to be compatible with T.38
+  */
+
+class OpalFaxAudioFormat : public OpalMediaFormat
+{
+  friend class OpalPluginCodecManager;
+    PCLASSINFO(OpalFaxAudioFormat, OpalMediaFormat);
+  public:
+    OpalFaxAudioFormat(
+      const char * fullName,    ///<  Full name of media format
+      RTP_DataFrame::PayloadTypes rtpPayloadType, ///<  RTP payload type code
+      const char * encodingName,///<  RTP encoding name
+      PINDEX   frameSize,       ///<  Size of frame in bytes (if applicable)
+      unsigned frameTime,       ///<  Time for frame in RTP units (if applicable)
+      unsigned rxFrames,        ///<  Maximum number of frames per packet we can receive
+      unsigned txFrames,        ///<  Desired number of frames per packet we transmit
+      unsigned maxFrames = 256, ///<  Maximum possible frames per packet
+      unsigned clockRate = 8000, ///<  Clock Rate 
+      time_t timeStamp = 0       ///<  timestamp (for versioning)
+    );
+};
+
+///////////////////////////////////////////////////////////////////////////////
+
+/**
+  *  This defines a pseudo RTP session used for T.38 channels
+  */
+/**This class is for the IETF Real Time Protocol interface on UDP/IP.
+ */
+class T38PseudoRTP : public RTP_UDP
+{
+  PCLASSINFO(T38PseudoRTP, RTP_UDP);
+
+  public:
+  /**@name Construction */
+  //@{
+    /**Create a new RTP channel.
+     */
+    T38PseudoRTP(
+      PHandleAggregator * aggregator, ///< RTP aggregator
+      unsigned id,                    ///<  Session ID for RTP channel
+      BOOL remoteIsNAT                ///<  TRUE is remote is behind NAT
+    );
+
+    /// Destroy the RTP
+    ~T38PseudoRTP();
+
+    BOOL ReadData(RTP_DataFrame & frame, BOOL loop);
+    BOOL WriteData(RTP_DataFrame & frame);
+    RTP_Session::SendReceiveStatus OnSendData(RTP_DataFrame & frame);
+    RTP_Session::SendReceiveStatus OnSendControl(RTP_ControlFrame & /*frame*/, PINDEX & /*len*/);
+
+    RTP_Session::SendReceiveStatus ReadDataPDU(RTP_DataFrame & frame);
+    RTP_Session::SendReceiveStatus OnReceiveData(RTP_DataFrame & frame);
+
+    BOOL SetRemoteSocketInfo(PIPSocket::Address address, WORD port, BOOL isDataPort);
+
+  protected:
+    int WaitForPDU(PUDPSocket & dataSocket, PUDPSocket & controlSocket, const PTimeInterval & timeout);
+    BOOL OnTimeout(RTP_DataFrame & frame);
+    BOOL corrigendumASN;
+    int consecutiveBadPackets;
+
+    PBYTEArray lastIFP;
+
+#if 0
+    PList<PBYTEArray> redundantIFPs;
+#endif
+
+  //@}
+};
+
+///////////////////////////////////////////////////////////////////////////////
+
+class OpalFaxCallInfo {
+  public:
+    OpalFaxCallInfo();
+    PUDPSocket socket;
+    PPipeChannel spanDSP;
+    unsigned refCount;
+    PIPSocket::Address spanDSPAddr;
+    WORD spanDSPPort;
+};
+
+///////////////////////////////////////////////////////////////////////////////
+
+/**This class describes a media stream that transfers data to/from a fax session
+  */
+class OpalFaxMediaStream : public OpalMediaStream
+{
+  PCLASSINFO(OpalFaxMediaStream, OpalMediaStream);
+  public:
+  /**@name Construction */
+  //@{
+    /**Construct a new media stream for T.38 sessions.
+      */
+    OpalFaxMediaStream(
+      OpalConnection & conn,
+      const OpalMediaFormat & mediaFormat, ///<  Media format for stream
+      unsigned sessionID, 
+      BOOL isSource ,                      ///<  Is a source stream
+      const PString & token,               ///<  token used to match incoming/outgoing streams
+      const PString & filename
+    );
+  //@}
+
+  /**@name Overrides of OpalMediaStream class */
+  //@{
+    /**Open the media stream using the media format.
+
+       The default behaviour simply sets the isOpen variable to TRUE.
+      */
+    virtual BOOL Open();
+
+    /**Close the media stream.
+
+       The default does nothing.
+      */
+    virtual BOOL Close();
+
+    /**Start the media stream.
+
+       The default behaviour calls Resume() on the associated OpalMediaPatch
+       thread if it was suspended.
+      */
+    virtual BOOL Start();
+
+    /**Read an RTP frame of data from the source media stream.
+       The new behaviour simply calls RTP_Session::ReadData().
+      */
+    virtual BOOL ReadPacket(
+      RTP_DataFrame & packet
+    );
+
+    /**Write an RTP frame of data to the sink media stream.
+       The new behaviour simply calls RTP_Session::WriteData().
+      */
+    virtual BOOL WritePacket(
+      RTP_DataFrame & packet
+    );
+
+    /**Indicate if the media stream is synchronous.
+       Returns FALSE for RTP streams.
+      */
+    virtual BOOL IsSynchronous() const;
+
+    virtual PString GetSpanDSPCommandLine(OpalFaxCallInfo &);
+
+  //@}
+
+  protected:
+    PMutex infoMutex;
+    PString sessionToken;
+    OpalFaxCallInfo * faxCallInfo;
+    PFilePath filename;
+};
+
+///////////////////////////////////////////////////////////////////////////////
+
+/**This class describes a media stream that transfers data to/from a T.38 session
+  */
+class OpalT38MediaStream : public OpalFaxMediaStream
+{
+  PCLASSINFO(OpalT38MediaStream, OpalFaxMediaStream);
+  public:
+    OpalT38MediaStream(
+      OpalConnection & conn,
+      const OpalMediaFormat & mediaFormat, ///<  Media format for stream
+      unsigned sessionID, 
+      BOOL isSource ,                      ///<  Is a source stream
+      const PString & token,               ///<  token used to match incoming/outgoing streams
+      const PString & filename             ///<  filename
+    );
+
+    PString GetSpanDSPCommandLine(OpalFaxCallInfo &);
+
+    BOOL ReadPacket(RTP_DataFrame & packet);
+    BOOL WritePacket(RTP_DataFrame & packet);
+};
+
+///////////////////////////////////////////////////////////////////////////////
+
+class OpalFaxConnection;
+
+/** Fax Endpoint.
+ */
+class OpalFaxEndPoint : public OpalEndPoint
+{
+  PCLASSINFO(OpalFaxEndPoint, OpalEndPoint);
+  public:
+  /**@name Construction */
+  //@{
+    /**Create a new endpoint.
+     */
+    OpalFaxEndPoint(
+      OpalManager & manager,      ///<  Manager of all endpoints.
+      const char * prefix = "fax" ///<  Prefix for URL style address strings
+    );
+
+    /**Destroy endpoint.
+     */
+    ~OpalFaxEndPoint();
+  //@}
+
+    virtual BOOL MakeConnection(
+      OpalCall & call,          ///<  Owner of connection
+      const PString & party,    ///<  Remote party to call
+      void * userData = NULL,          ///<  Arbitrary data to pass to connection
+      unsigned int options = 0,     ///<  options to pass to conneciton
+      OpalConnection::StringOptions * stringOptions = NULL
+    );
+
+    /**Create a connection for the fax endpoint.
+      */
+    virtual OpalFaxConnection * CreateConnection(
+      OpalCall & call,          ///< Owner of connection
+      const PString & filename, ///< filename to send/receive
+      void * userData = NULL,   ///< Arbitrary data to pass to connection
+      OpalConnection::StringOptions * stringOptions = NULL
+    );
+
+    /**Get the data formats this endpoint is capable of operating.
+       This provides a list of media data format names that may be used by an
+       OpalMediaStream may be created by a connection from this endpoint.
+
+       Note that a specific connection may not actually support all of the
+       media formats returned here, but should return no more.
+
+       The default behaviour is pure.
+      */
+    virtual OpalMediaFormatList GetMediaFormats() const;
+
+    virtual PString MakeToken();
+
+  /**@name User Interface operations */
+    /**Accept the incoming connection.
+      */
+    virtual void AcceptIncomingConnection(
+      const PString & connectionToken ///<  Token of connection to accept call
+    );
+
+    /**Call back when patching a media stream.
+       This function is called when a connection has created a new media
+       patch between two streams.
+      */
+    virtual void OnPatchMediaStream(
+      const OpalFaxConnection & connection, ///<  Connection having new patch
+      BOOL isSource,                         ///<  Source patch
+      OpalMediaPatch & patch                 ///<  New patch
+    );
+  //@}
+};
+
+///////////////////////////////////////////////////////////////////////////////
+
+/** Fax Connection
+ */
+class OpalFaxConnection : public OpalConnection
+{
+  PCLASSINFO(OpalFaxConnection, OpalConnection);
+  public:
+  /**@name Construction */
+  //@{
+    /**Create a new endpoint.
+     */
+    OpalFaxConnection(
+      OpalCall & call,                 ///<  Owner calll for connection
+      OpalFaxEndPoint & endpoint,      ///<  Owner endpoint for connection
+      const PString & filename,        ///<  filename to send/receive
+      const PString & _token,           ///<  token for connection
+      OpalConnection::StringOptions * stringOptions = NULL
+    );
+
+    /**Destroy endpoint.
+     */
+    ~OpalFaxConnection();
+  //@}
+
+  /**@name Overrides from OpalConnection */
+  //@{
+    /**Start an outgoing connection.
+       This function will initiate the connection to the remote entity, for
+       example in H.323 it sends a SETUP, in SIP it sends an INVITE etc.
+
+       The default behaviour does.
+      */
+    virtual BOOL SetUpConnection();
+
+    /**Indicate to remote endpoint an alert is in progress.
+       If this is an incoming connection and the AnswerCallResponse is in a
+       AnswerCallDeferred or AnswerCallPending state, then this function is
+       used to indicate to that endpoint that an alert is in progress. This is
+       usually due to another connection which is in the call (the B party)
+       has received an OnAlerting() indicating that its remote endpoint is
+       "ringing".
+
+       The default behaviour does nothing.
+      */
+    virtual BOOL SetAlerting(
+      const PString & calleeName,   ///<  Name of endpoint being alerted.
+      BOOL withMedia                ///<  Open media with alerting
+    );
+
+    /**Indicate to remote endpoint we are connected.
+
+       The default behaviour does nothing.
+      */
+    virtual BOOL SetConnected();
+
+    /**Get the data formats this connection is capable of operating.
+       This provides a list of media data format names that an
+       OpalMediaStream may be created in within this connection.
+
+       The default behaviour returns the formats the PSoundChannel can do,
+       typically only PCM-16.
+      */
+    virtual OpalMediaFormatList GetMediaFormats() const;
+
+    OpalMediaStream * CreateMediaStream(const OpalMediaFormat & mediaFormat, unsigned sessionID, BOOL isSource);
+
+    /**Call back when patching a media stream.
+       This function is called when a connection has created a new media
+       patch between two streams.
+       Add the echo canceler patch and call the endpoint function of
+       the same name.
+       Add a PCM silence detector filter.
+      */
+    virtual void OnPatchMediaStream(
+      BOOL isSource,
+      OpalMediaPatch & patch    ///<  New patch
+    );
+
+    /**Open source transmitter media stream for session.
+      */
+    virtual BOOL OpenSourceMediaStream(
+      const OpalMediaFormatList & mediaFormats, ///<  Optional media format to open
+      unsigned sessionID                   ///<  Session to start stream on
+    );
+
+    /**Open source transmitter media stream for session.
+      */
+    virtual OpalMediaStream * OpenSinkMediaStream(
+      OpalMediaStream & source    ///<  Source media sink format to open to
+    );
+
+  /**@name New operations */
+  //@{
+    /**Accept the incoming connection.
+      */
+    virtual void AcceptIncoming();
+
+  //@}
+
+    void AdjustMediaFormats(OpalMediaFormatList & mediaFormats) const;
+
+  protected:
+    OpalFaxEndPoint & endpoint;
+    PString filename;
+    BOOL forceFaxAudio;
+};
+
+/////////////////////////////////////////////////////////////////////////////////
+
+class OpalT38Connection;
+
+/** T.38 Endpoint
+ */
+class OpalT38EndPoint : public OpalFaxEndPoint
+{
+  PCLASSINFO(OpalT38EndPoint, OpalFaxEndPoint);
+  public:
+  /**@name Construction */
+  //@{
+    /**Create a new endpoint.
+     */
+    OpalT38EndPoint(
+      OpalManager & manager,      ///<  Manager of all endpoints.
+      const char * prefix = "t38" ///<  Prefix for URL style address strings
+    );
+    OpalMediaFormatList GetMediaFormats() const;
+    PString MakeToken();
+    virtual OpalFaxConnection * CreateConnection(OpalCall & call, const PString & filename, void * /*userData*/, OpalConnection::StringOptions * stringOptions);
+};
+
+///////////////////////////////////////////////////////////////////////////////
+
+/** T.38 Connection
+ */
+class OpalT38Connection : public OpalFaxConnection
+{
+  PCLASSINFO(OpalT38Connection, OpalFaxConnection);
+  public:
+  /**@name Construction */
+  //@{
+    /**Create a new endpoint.
+     */
+    OpalT38Connection(
+      OpalCall & call,                 ///<  Owner calll for connection
+      OpalT38EndPoint & endpoint,      ///<  Owner endpoint for connection
+      const PString & filename,        ///<  filename to send/receive
+      const PString & _token,           ///<  token for connection
+      OpalConnection::StringOptions * stringOptions = NULL
+    );
+    void AdjustMediaFormats(OpalMediaFormatList & mediaFormats) const;
+    OpalMediaStream * CreateMediaStream(const OpalMediaFormat & mediaFormat, unsigned sessionID, BOOL isSource);
+};
+
+///////////////////////////////////////////////////////////////////////////////
+
+#define OPAL_T38            "T.38"
+#define OPAL_PCM16_FAX      "PCM-16-Fax"
+
+extern const OpalFaxAudioFormat & GetOpalPCM16Fax();
+extern const OpalMediaFormat    & GetOpalT38();
+
+#define OpalPCM16Fax          GetOpalPCM16Fax()
+#define OpalT38               GetOpalT38()
+
+
 #endif // __OPAL_T38PROTO_H
 
 
-/////////////////////////////////////////////////////////////////////////////
