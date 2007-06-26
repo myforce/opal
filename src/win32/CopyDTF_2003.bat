7ba2d2505e350a82df7c74ce3981b4fd816eeed6
Robert Jongbloed <robertj@voxlucida.com.au>
2007-06-26 11:42:16 +0000

    Update symbols

---
+++ src/win32/CopyDTF_2003.bat
@@ -0,0 +1,6 @@
+ren opald_2003.dtf opald_2003_old.dtf
+copy ..\..\lib\debug\opald.def opald_2003.dtf
+ren opal_2003.dtf opal_2003_old.dtf
+copy ..\..\lib\release\opal.def opal_2003.dtf
+ren opaln_2003.dtf opaln_2003_old.dtf
+copy ..\..\lib\notrace\opaln.def opaln_2003.dtf
