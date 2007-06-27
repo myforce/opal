b18b5126c01e3d922473b0b707ae0df02fa7ae6e
Robert Jongbloed <robertj@voxlucida.com.au>
2007-06-27 08:14:25 +0000

    Update symbols.

---
+++ src/win32/CopyDTF_2005.bat
@@ -0,0 +1,7 @@
+mkdir backup
+move opald_2005.dtf backup\
+copy ..\..\lib\debug\opald.def opald_2005.dtf
+move opal_2005.dtf backup\
+copy ..\..\lib\release\opal.def opal_2005.dtf
+move opaln_2005.dtf backup\
+copy ..\..\lib\notrace\opaln.def opaln_2005.dtf
