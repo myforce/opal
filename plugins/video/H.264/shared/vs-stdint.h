a4a2097a18ecbb9d41604f41c737824ba91f1591
Simon Horne <shorne@isvo.net>
2007-08-19 18:57:33 +0000

    H323 Macro and VS Bug fixes
    git-svn-id: svn://svn.code.sf.net/p/opalvoip/code/opal/trunk@18207 023b2edf-31b2-4de3-b41e-bca80c47788f

--- plugins/video/H.264/shared/vs-stdint.h
+++ plugins/video/H.264/shared/vs-stdint.h
@@ -56,11 +56,11 @@
 // 7.18.1 Integer types
 
 // 7.18.1.1 Exact-width integer types
-typedef __int8            int8_t;
+typedef signed char       int8_t;  // <-- Fix for conflict with AVCODEC definition
 typedef __int16           int16_t;
 typedef __int32           int32_t;
 typedef __int64           int64_t;
-typedef unsigned __int8   uint8_t;
+typedef unsigned char     uint8_t; // <-- Fix for conflict with AVCODEC definition
 typedef unsigned __int16  uint16_t;
 typedef unsigned __int32  uint32_t;
 typedef unsigned __int64  uint64_t;
