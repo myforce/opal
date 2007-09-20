e78cc0543e996fa79edc13ea7d642ae8b7b48615
Robert Jongbloed <robertj@voxlucida.com.au>
2007-09-20 00:34:39 +0000

    Moved samples binary to common directory to make it easier to clean. Fixed "No Trace" builds.
    git-svn-id: svn://svn.code.sf.net/p/opalvoip/code/opal/trunk@18528 023b2edf-31b2-4de3-b41e-bca80c47788f

--- src/win32/CopyDTF_2003.bat
+++ src/win32/CopyDTF_2003.bat
@@ -4,4 +4,4 @@ copy ..\..\lib\debug\opald.def opald_2003.dtf
 move opal_2003.dtf backup\
 copy ..\..\lib\release\opal.def opal_2003.dtf
 move opaln_2003.dtf backup\
-copy ..\..\lib\notrace\opaln.def opaln_2003.dtf
+copy "..\..\lib\no trace\opaln.def" opaln_2003.dtf
