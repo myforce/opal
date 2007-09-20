a681db43d62c0ed65d22d12798b29647baa70c84
Robert Jongbloed <robertj@voxlucida.com.au>
2007-09-20 00:54:07 +0000

    Fixed correct codec version loading.
    git-svn-id: svn://svn.code.sf.net/p/opalvoip/code/opal/trunk@18534 023b2edf-31b2-4de3-b41e-bca80c47788f

--- samples/opalcodecinfo/main.cxx
+++ samples/opalcodecinfo/main.cxx
@@ -22,6 +22,9 @@
  * Contributor(s): ______________________________________.
  *
  * $Log: main.cxx,v $
+ * Revision 1.13  2007/09/20 00:54:07  rjongbloed
+ * Fixed correct codec version loading.
+ *
  * Revision 1.12  2007/09/20 00:34:37  rjongbloed
  * Moved samples binary to common directory to make it easier to clean.
  * Fixed "No Trace" builds.
@@ -461,7 +464,7 @@ void DisplayPlugInInfo(const PString & name, const PPluginModuleManager::PluginL
         return;
       }
       unsigned int count;
-      PluginCodec_Definition * codecs = (*getCodecs)(&count, PLUGIN_CODEC_VERSION_VIDEO);
+      PluginCodec_Definition * codecs = (*getCodecs)(&count, PLUGIN_CODEC_VERSION_OPTIONS);
       if (codecs == NULL || count == 0) {
         cout << "error: " << name << " does not define any codecs for this version of the plugin API" << endl;
         return;
