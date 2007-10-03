62f6c4842a31bc118eacab70d28147e7e0057c2c
Robert Jongbloed <robertj@voxlucida.com.au>
2007-10-03 01:49:14 +0000

    Fixed build for Windows Mobile 5 and added Windows Mobile 6
    git-svn-id: svn://svn.code.sf.net/p/opalvoip/code/opal/trunk@18600 023b2edf-31b2-4de3-b41e-bca80c47788f

--- src/win32/CopyDTF_2005.bat
+++ src/win32/CopyDTF_2005.bat
@@ -1,7 +1,24 @@
 mkdir backup
+
 move opald_2005.dtf backup\
 copy ..\..\lib\debug\opald.def opald_2005.dtf
 move opal_2005.dtf backup\
 copy ..\..\lib\release\opal.def opal_2005.dtf
 move opaln_2005.dtf backup\
-copy ..\..\lib\notrace\opaln.def opaln_2005.dtf
+copy "..\..\lib\no trace\opaln.def" opaln_2005.dtf
+
+move opald_2005_wm.dtf backup\
+copy ..\..\lib\wm5\debug\opald.def opald_2005_wm.dtf
+move opal_2005_wm.dtf backup\
+copy ..\..\lib\wm5\release\opal.def opal_2005_wm.dtf
+move opaln_2005_wm.dtf backup\
+copy "..\..\lib\wm5\no trace\opaln.def" opaln_2005_wm.dtf
+
+move opald_2005_wm6.dtf backup\
+copy ..\..\lib\wm6\debug\opald.def opald_2005_wm6.dtf
+move opal_2005_wm6.dtf backup\
+copy ..\..\lib\wm6\release\opal.def opal_2005_wm6.dtf
+move opaln_2005_wm6.dtf backup\
+copy "..\..\lib\wm6\no trace\opaln.def" opaln_2005_wm6.dtf
+
+pause
