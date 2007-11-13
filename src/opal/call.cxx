d9308c58ad121db24429dded2bc9d886fea13e26
dsandras <dsandras@023b2edf-31b2-4de3-b41e-bca80c47788f>
2007-11-13 19:28:56 +0000

    Made GCC happy with the new changes.

--- src/opal/call.cxx
+++ src/opal/call.cxx
@@ -596,8 +596,10 @@ BOOL OpalCall::OpenSourceMediaStreams(const OpalConnection & connection,
   // media formats to match that connection so we get symmetric codecs
   if (connectionsActive.GetSize() > 0) {
     OpalMediaStream * strm = connection.GetMediaStream(sessionID, TRUE);
-    if (strm != NULL)
-      adjustableMediaFormats.Reorder((PString)strm->GetMediaFormat());
+    if (strm != NULL) {
+      PString format = strm->GetMediaFormat();
+      adjustableMediaFormats.Reorder(format);
+    }
   }
   
   for (PSafePtr<OpalConnection> conn(connectionsActive, PSafeReadOnly); conn != NULL; ++conn) {
