339be91d9c6f00ae35f374f17628c6b08e34e16c
ms30002000 <unknown@opalvoip.org>
2007-12-26 12:27:37 +0000

    H.264: Added tables for resolutions and levels Dump profile, level and constraints when sending/receiving SPS NAL units
    git-svn-id: svn://svn.code.sf.net/p/opalvoip/code/opal/trunk@19158 023b2edf-31b2-4de3-b41e-bca80c47788f

--- plugins/video/H.264/shared/h264frame.h
+++ plugins/video/H.264/shared/h264frame.h
@@ -33,6 +33,10 @@
 #ifndef __H264FRAME_H__
 #define __H264FRAME_H__ 1
 
+// GVX 3000 does not like STAP packets... So we waste 40 bytes per connection...
+//#define SEND_STAP_PACKETS 1
+
+
 #ifdef _MSC_VER
 #include "../../common/vs-stdint.h"
 #include "../../common/rtpframe.h"
@@ -40,7 +44,6 @@
 #include <stdint.h>
 #include "rtpframe.h"
 #endif
-
 
 #define H264_NAL_TYPE_NON_IDR_SLICE 1
 #define H264_NAL_TYPE_DP_A_SLICE 2
@@ -56,8 +59,61 @@
 #define H264_NAL_TYPE_FILLER_DATA 0xc
 #define H264_NAL_TYPE_SEQ_EXTENSION 0xd
 
-// GVX 3000 does not like STAP packets... So we waste 40 bytes per connection...
-//#define SEND_STAP_PACKETS 1
+const static struct h264_level {
+    unsigned level_idc;
+    unsigned mbps;        /* max macroblock processing rate (macroblocks/sec) */
+    unsigned frame_size;  /* max frame size (macroblocks) */
+    unsigned dpb;         /* max decoded picture buffer (bytes) */
+    long unsigned bitrate;     /* max bitrate (kbit/sec) */
+    int cpb;         /* max vbv buffer (kbit) */
+    int mv_range;    /* max vertical mv component range (pixels) */
+    int mvs_per_2mb; /* max mvs per 2 consecutive mbs. */
+    int slice_rate;
+    int bipred8x8;   /* limit bipred to >=8x8 */
+    int direct8x8;   /* limit b_direct to >=8x8 */
+    int frame_only;  /* forbid interlacing */
+} h264_levels[] = {
+    { 10,   1485,    99,   152064,     64000,    175,  64, 64,  0, 0, 0, 1 },
+//  {"1b",  1485,    99,   152064,    128000,    350,  64, 64,  0, 0, 0, 1 },
+    { 11,   3000,   396,   345600,    192000,    500, 128, 64,  0, 0, 0, 1 },
+    { 12,   6000,   396,   912384,    384000,   1000, 128, 64,  0, 0, 0, 1 },
+    { 13,  11880,   396,   912384,    768000,   2000, 128, 64,  0, 0, 0, 1 },
+    { 20,  11880,   396,   912384,   2000000,   2000, 128, 64,  0, 0, 0, 1 },
+    { 21,  19800,   792,  1824768,   4000000,   4000, 256, 64,  0, 0, 0, 0 },
+    { 22,  20250,  1620,  3110400,   4000000,   4000, 256, 64,  0, 0, 0, 0 },
+    { 30,  40500,  1620,  3110400,  10000000,  10000, 256, 32, 22, 0, 1, 0 },
+    { 31, 108000,  3600,  6912000,  14000000,  14000, 512, 16, 60, 1, 1, 0 },
+    { 32, 216000,  5120,  7864320,  20000000,  20000, 512, 16, 60, 1, 1, 0 },
+    { 40, 245760,  8192, 12582912,  20000000,  25000, 512, 16, 60, 1, 1, 0 },
+    { 41, 245760,  8192, 12582912,  50000000,  62500, 512, 16, 24, 1, 1, 0 },
+    { 42, 522240,  8704, 13369344,  50000000,  62500, 512, 16, 24, 1, 1, 1 },
+    { 50, 589824, 22080, 42393600, 135000000, 135000, 512, 16, 24, 1, 1, 1 },
+    { 51, 983040, 36864, 70778880, 240000000, 240000, 512, 16, 24, 1, 1, 1 },
+    { 0 }
+};
+
+// This table is used in order to select a different resolution if the desired one
+// consists of more macroblocks than the level limit
+const static struct h264_resolution {
+    unsigned width;
+    unsigned height;
+    unsigned macroblocks;
+} h264_resolutions[] = {
+    { 1920, 1088, 8160 },
+    { 1600, 1200, 7500 },
+    { 1408, 1152, 6336 },
+    { 1280, 1024, 5120 },
+    { 1280,  720, 3600 },
+    { 1024,  768, 3072 },
+    {  800,  600, 1900 },
+    {  704,  576, 1584 },
+    {  640,  480, 1200 },
+    {  352,  288,  396 },
+    {  320,  240,  300 },
+    {  176,  144,   99 },
+    {  128,   96,   48 },
+    { 0 }
+};
 
 #ifndef LICENCE_MPL
 extern "C"
