ec690a3aeb464166025a0f84b0c24d38c303f1b2
Robert Jongbloed <robertj@voxlucida.com.au>
2008-02-18 06:53:42 +0000

    Update symbols
    git-svn-id: svn://svn.code.sf.net/p/opalvoip/code/opal/trunk@19539 023b2edf-31b2-4de3-b41e-bca80c47788f

--- src/win32/CopyDTF_2005.bat
+++ src/win32/CopyDTF_2005.bat
@@ -8,17 +8,17 @@ move opaln_2005.dtf backup\
 copy "..\..\lib\no trace\opaln.def" opaln_2005.dtf
 
 move opald_2005_wm.dtf backup\
-copy ..\..\lib\wm5\debug\opald.def opald_2005_wm.dtf
+copy ..\..\lib\wm5ppc\debug\opald.def opald_2005_wm.dtf
 move opal_2005_wm.dtf backup\
-copy ..\..\lib\wm5\release\opal.def opal_2005_wm.dtf
+copy ..\..\lib\wm5ppc\release\opal.def opal_2005_wm.dtf
 move opaln_2005_wm.dtf backup\
-copy "..\..\lib\wm5\no trace\opaln.def" opaln_2005_wm.dtf
+copy "..\..\lib\wm5ppc\no trace\opaln.def" opaln_2005_wm.dtf
 
 move opald_2005_wm6.dtf backup\
-copy ..\..\lib\wm6\debug\opald.def opald_2005_wm6.dtf
+copy ..\..\lib\wm6pro\debug\opald.def opald_2005_wm6.dtf
 move opal_2005_wm6.dtf backup\
-copy ..\..\lib\wm6\release\opal.def opal_2005_wm6.dtf
+copy ..\..\lib\wm6pro\release\opal.def opal_2005_wm6.dtf
 move opaln_2005_wm6.dtf backup\
-copy "..\..\lib\wm6\no trace\opaln.def" opaln_2005_wm6.dtf
+copy "..\..\lib\wm6pro\no trace\opaln.def" opaln_2005_wm6.dtf
 
 pause
