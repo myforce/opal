87695c08752b4f4efb2022418d2c50d300fc8b0d
Robert Jongbloed <robertj@voxlucida.com.au>
2008-09-17 08:15:24 +0000

    Update version number
    git-svn-id: svn://svn.code.sf.net/p/opalvoip/code/opal/trunk@21042 023b2edf-31b2-4de3-b41e-bca80c47788f

--- version.h
+++ version.h
@@ -35,9 +35,9 @@
  */
 
 #define MAJOR_VERSION 3
-#define MINOR_VERSION 3
+#define MINOR_VERSION 5
 #define BUILD_TYPE BetaCode
-#define BUILD_NUMBER 2
+#define BUILD_NUMBER 0
 
 
 #endif  // _OPAL_VERSION_H
