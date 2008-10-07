33a79fe7d7bee63e53684316c011651fd1837a96
Craig Southeren <craigs@postincrement.com>
2008-10-07 11:22:05 +0000

    Added copyright and license information
    git-svn-id: svn://svn.code.sf.net/p/opalvoip/code/opal/trunk@21257 023b2edf-31b2-4de3-b41e-bca80c47788f

--- plugins/video/H.263-1998/rfc2190.cxx
+++ plugins/video/H.263-1998/rfc2190.cxx
@@ -1,4 +1,21 @@
 /*
+ * RFC 2190 packetiser and unpacketiser 
+ *
+ * Copyright (C) 2008 Post Increment
+ *
+ * The contents of this file are subject to the Mozilla Public License
+ * Version 1.0 (the "License"); you may not use this file except in
+ * compliance with the License. You may obtain a copy of the License at
+ * http://www.mozilla.org/MPL/
+ *
+ * Software distributed under the License is distributed on an "AS IS"
+ * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See
+ * the License for the specific language governing rights and limitations
+ * under the License.
+ *
+ * The Original Code is Opal
+ *
+ * Contributor(s): Craig Southeren <craigs@postincrement.com>
  *
  */
 
