091088ce3c87fafb45536751b7105d525f88beab
Robert Jongbloed <robertj@voxlucida.com.au>
2008-10-07 13:03:26 +0000

    Reversed stupid change
    git-svn-id: svn://svn.code.sf.net/p/opalvoip/code/opal/trunk@21260 023b2edf-31b2-4de3-b41e-bca80c47788f

--- samples/openphone/main.cxx
+++ samples/openphone/main.cxx
@@ -4835,7 +4835,7 @@ double StatisticsField::CalculateBandwidth(DWORD bytes)
 
   double value;
   if (m_lastTick != 0)
-    value = (bytes - m_lastBytes) / (double)(tick - m_lastTick).GetMilliSeconds(); // Ends up as kilobits/second
+    value = 8.0 * (bytes - m_lastBytes) / (tick - m_lastTick).GetMilliSeconds(); // Ends up as kilobits/second
   else
     value = 0;
 
