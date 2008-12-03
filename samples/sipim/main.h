6e542ca3dd2c2d4e1fbe2fb006db69f5438703e4
Craig Southeren <craigs@postincrement.com>
2008-12-03 04:50:15 +0000

    Add beginnings of SIP IM test program
    git-svn-id: svn://svn.code.sf.net/p/opalvoip/code/opal/trunk@21688 023b2edf-31b2-4de3-b41e-bca80c47788f

---
+++ samples/sipim/main.h
@@ -0,0 +1,77 @@
+/*
+ * main.h
+ *
+ * OPAL application source file for seing IM via SIP
+ *
+ * Copyright (c) 2008 Post Increment
+ *
+ * The contents of this file are subject to the Mozilla Public License
+ * Version 1.0 (the "License"); you may not use this file except in
+ * compliance with the License. You may obtain a copy of the License at
+ * http://www.mozilla.org/MPL/
+ *
+ * Software distributed under the License is distributed on an "AS IS"
+ * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See
+ * the License for the specific language governing rights and limitations
+ * under the License.
+ *
+ * The Original Code is Open Phone Abstraction Library.
+ *
+ * The Initial Developer of the Original Code is Equivalence Pty. Ltd.
+ *
+ * Contributor(s): ______________________________________.
+ *
+ * $Revision: 20534 $
+ * $Author: rjongbloed $
+ * $Date: 2008-07-04 13:47:05 +1000 (Fri, 04 Jul 2008) $
+ */
+
+#ifndef _SipIM_MAIN_H
+#define _SipIM_MAIN_H
+
+#include <opal/manager.h>
+#include <sip/sipep.h>
+
+
+class MyManager : public OpalManager
+{
+    PCLASSINFO(MyManager, OpalManager)
+
+  public:
+    virtual void OnClearedCall(OpalCall & call); // Callback override
+
+    PSyncPoint m_completed;
+};
+
+class MyPCSSEndPoint : public OpalPCSSEndPoint
+{
+  PCLASSINFO(MyPCSSEndPoint, OpalPCSSEndPoint);
+
+  public:
+    MyPCSSEndPoint(MyManager & manager)
+      : OpalPCSSEndPoint(manager)
+    { }
+
+    virtual PBoolean OnShowIncoming(const OpalPCSSConnection & connection);
+    virtual PBoolean OnShowOutgoing(const OpalPCSSConnection & connection);
+};
+
+class SipIM : public PProcess
+{
+    PCLASSINFO(SipIM, PProcess)
+
+  public:
+    SipIM();
+    ~SipIM();
+
+    virtual void Main();
+
+  private:
+    MyManager * m_manager;
+};
+
+
+#endif  // _SipIM_MAIN_H
+
+
+// End of File ///////////////////////////////////////////////////////////////
