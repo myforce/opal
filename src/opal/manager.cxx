791bee861a4fc99deddf8e0f131aa429515bdd52
Robert Jongbloed <robertj@voxlucida.com.au>
2008-12-10 05:01:35 +0000

    Added common video format names to default media format order.

--- src/opal/manager.cxx
+++ src/opal/manager.cxx
@@ -63,7 +63,11 @@ static const char * const DefaultMediaFormatOrder[] = {
   OPAL_GSM0610,
   OPAL_G728,
   OPAL_G711_ULAW_64K,
-  OPAL_G711_ALAW_64K
+  OPAL_G711_ALAW_64K,
+  OPAL_H264,
+  OPAL_MPEG4,
+  OPAL_H263,
+  OPAL_H261
 };
 
 #define new PNEW
