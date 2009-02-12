78af4a4bab9fbd48b1b1f130367e306099fcec09
Craig Southeren <craigs@postincrement.com>
2009-02-12 07:30:50 +0000

    Backport (up-port?) rate control scaler controls

--- include/opal/mediafmt.h
+++ include/opal/mediafmt.h
@@ -1139,7 +1139,7 @@ class OpalVideoFormat : public OpalMediaFormat
     static const PString & RateControlEnableOption();
     static const PString & RateControlWindowSizeOption();
     static const PString & RateControlMaxFramesSkipOption();
-    static const PString & RateControllerBitRateOption();
+    static const PString & RateControllerBitRateScalerOption();
 };
 #endif
 
