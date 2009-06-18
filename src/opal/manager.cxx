d61496a1d5c142459a74df1fc1ed7e5bc1f82c03
Craig Southeren <craigs@postincrement.com>
2009-06-18 16:14:50 +0000

    Handle case where call hangs up very, very quickly, or a call takes a long, long time to setup and then failed. In either case, MakeCall can return without any connections in a call

--- src/opal/manager.cxx
+++ src/opal/manager.cxx
@@ -374,15 +374,22 @@ PBoolean OpalManager::SetUpCall(const PString & partyA,
   // go through the routing engine via OnIncomingConnection. If we were the
   // B-Party then SetUpConnection() gets called in the context of the A-party
   // thread.
-  if (MakeConnection(*call, partyA, userData, options, stringOptions) && call->GetConnection(0)->SetUpConnection()) {
-    PTRACE(3, "OpalMan\tSetUpCall succeeded, call=" << *call);
-    return true;
-  }
-
+  bool r = MakeConnection(*call, partyA, userData, options, stringOptions);
   PSafePtr<OpalConnection> connection = call->GetConnection(0);
   OpalConnection::CallEndReason endReason = OpalConnection::NumCallEndReasons;
-  if (connection != NULL)
+
+  if (r) {
+    if (connection == NULL) {
+      PTRACE(3, "OpalMan\tSetUpCall failed because call was shutdown");
+      endReason = call->GetCallEndReason();
+    }
+    else if (connection->SetUpConnection()) {
+      PTRACE(3, "OpalMan\tSetUpCall succeeded, call=" << *call);
+      return true;
+    }
     endReason = connection->GetCallEndReason();
+  }
+
   if (endReason == OpalConnection::NumCallEndReasons)
     endReason = OpalConnection::EndedByTemporaryFailure;
   call->Clear(endReason);
