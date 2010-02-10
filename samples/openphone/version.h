4c4373e114a6c17721fd4e808dbbaec63102edef
Robert Jongbloed <robertj@voxlucida.com.au>
2010-02-10 23:59:12 +0000

    Link OpenPhone version to the OPAL version, so always in lock step.

--- samples/openphone/version.h
+++ samples/openphone/version.h
@@ -32,16 +32,18 @@
 #ifndef _OpenPhone_VERSION_H
 #define _OpenPhone_VERSION_H
 
+#include <opal/buildopts.h>
+
 #define	PRODUCT_NAME_TEXT	"Open Phone"
 #define	EXE_NAME_TEXT	        "openphone"
 #define	MANUFACTURER_TEXT	"Vox Lucida"
 #define	COPYRIGHT_HOLDER	"Vox Lucida Pty. Ltd. (Australia)"
 
 
-#define MAJOR_VERSION 3
-#define MINOR_VERSION 7
+#define MAJOR_VERSION OPAL_MAJOR
+#define MINOR_VERSION OPAL_MINOR
 #define BUILD_TYPE    BetaCode
-#define BUILD_NUMBER 1
+#define BUILD_NUMBER  OPAL_BUILD
 
 
 #endif  // _OpenPhone_VERSION_H
