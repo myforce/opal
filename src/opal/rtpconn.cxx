baf429b8edd00339d39e4f45c659b51cb3163cc7
Craig Southeren <craigs@postincrement.com>
2010-06-01 06:07:48 +0000

    Fix compile warning on Windows
    git-svn-id: svn://svn.code.sf.net/p/opalvoip/code/opal/trunk@24433 023b2edf-31b2-4de3-b41e-bca80c47788f

--- src/opal/rtpconn.cxx
+++ src/opal/rtpconn.cxx
@@ -532,8 +532,17 @@ OpalRTPSessionManager::OpalRTPSessionManager(OpalRTPConnection & connection)
 
 
 OpalRTPSessionManager::OpalRTPSessionManager(const OpalRTPSessionManager & other)
+#ifdef _MSC_VER
+#pragma warning(disable:4355)
+#endif
   : PObject(*this)
-  , m_connection(other.m_connection)
+  ,
+#ifdef _MSC_VER
+#pragma warning(default:4355)
+#else
+  :
+#endif
+  m_connection(other.m_connection)
   , sessions(other.sessions)
 {
   m_connection.m_allSessions.push_back(this);
