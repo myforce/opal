dac38b0839b432d4357002ca3350e6dcdde4300a
Robert Jongbloed <robertj@voxlucida.com.au>
2010-09-27 01:37:13 +0000

    Fix for ID: 3074988 "Cseq value is empty, malformed packet as per Wireshark", reported anonymously.
    When we get an unsupported command, the headers were not read in which meant the 100 response could not be properly built. Header reading was made earlier in processing.
    
    git-svn-id: svn://svn.code.sf.net/p/opalvoip/code/opal/trunk@24740 023b2edf-31b2-4de3-b41e-bca80c47788f

--- src/sip/sippdu.cxx
+++ src/sip/sippdu.cxx
@@ -2032,9 +2032,9 @@ SIP_PDU::StatusCodes SIP_PDU::Read(OpalTransport & transport)
 
   // get the message from transport/datagram into cmd and parse MIME
   PString cmd;
-  *stream >> cmd;
+  *stream >> cmd >> m_mime;
 
-  if (!stream->good() || cmd.IsEmpty()) {
+  if (!stream->good() || cmd.IsEmpty() || m_mime.IsEmpty()) {
     if (stream == &datagram) {
       transport.setstate(ios::failbit);
       PTRACE(1, "SIP\tInvalid datagram from " << transport.GetLastReceivedAddress()
@@ -2083,14 +2083,6 @@ SIP_PDU::StatusCodes SIP_PDU::Read(OpalTransport & transport)
 
   if (m_versionMajor < 2) {
     PTRACE(2, "SIP\tInvalid version (" << m_versionMajor << ") received on " << transport);
-    return SIP_PDU::Failure_BadRequest;
-  }
-
-  // Getthe MIME fields
-  *stream >> m_mime;
-  if (!stream->good() || m_mime.IsEmpty()) {
-    PTRACE(2, "SIP\tInvalid MIME received on " << transport);
-    transport.clear(); // Clear flags so BadRequest response is sent by caller
     return SIP_PDU::Failure_BadRequest;
   }
 
