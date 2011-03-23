2731c70ebb2bd8e4731557575976706f4d8f4de8
Robert Jongbloed <robertj@voxlucida.com.au>
2011-03-23 10:42:26 +0000

    Added separate dialog text edit box in OpenPhone for registrar proxy

--- samples/openphone/main.cxx
+++ samples/openphone/main.cxx
@@ -242,6 +242,7 @@ DEF_FIELD(RegistrarContact);
 DEF_FIELD(RegistrarAuthID);
 DEF_FIELD(RegistrarUsername);
 DEF_FIELD(RegistrarPassword);
+DEF_FIELD(RegistrarProxy);
 DEF_FIELD(RegistrarTimeToLive);
 DEF_FIELD(RegistrarCompatibility);
 
@@ -1198,7 +1199,8 @@ bool MyManager::Initialise()
   if (config->Read(RegistrarUsedKey, &registration.m_Active, false) &&
       config->Read(RegistrarNameKey, &registration.m_Domain) &&
       config->Read(RegistrarUsernameKey, &registration.m_User) &&
-      config->Read(RegistrarPasswordKey, &registration.m_Password))
+      config->Read(RegistrarPasswordKey, &registration.m_Password) &&
+      config->Read(RegistrarProxyKey, &registration.m_Proxy))
     m_registrations.push_back(registration);
 
   config->SetPath(RegistrarGroup);
@@ -3473,6 +3475,7 @@ bool RegistrationInfo::Read(wxConfigBase & config)
   config.Read(RegistrarContactKey, &m_Contact);
   config.Read(RegistrarAuthIDKey, &m_AuthID);
   config.Read(RegistrarPasswordKey, &m_Password);
+  config.Read(RegistrarProxyKey, &m_Proxy);
   config.Read(RegistrarTimeToLiveKey, &m_TimeToLive);
   if (config.Read(RegistrarCompatibilityKey, &iType, SIPRegister::e_FullyCompliant))
     m_Compatibility = (SIPRegister::CompatibilityModes)iType;
@@ -3490,6 +3493,7 @@ void RegistrationInfo::Write(wxConfigBase & config)
   config.Write(RegistrarContactKey, m_Contact);
   config.Write(RegistrarAuthIDKey, m_AuthID);
   config.Write(RegistrarPasswordKey, m_Password);
+  config.Write(RegistrarProxyKey, m_Proxy);
   config.Write(RegistrarTimeToLiveKey, m_TimeToLive);
   config.Write(RegistrarCompatibilityKey, (int)m_Compatibility);
 }
@@ -3523,6 +3527,7 @@ bool RegistrationInfo::Start(SIPEndPoint & sipEP)
         param.m_contactAddress = m_Contact.p_str();
         param.m_authID = m_AuthID.p_str();
         param.m_password = m_Password.p_str();
+        param.m_proxyAddress = m_Proxy.p_str();
         param.m_expire = m_TimeToLive;
         param.m_compatibility = m_Compatibility;
         status = sipEP.Register(param, m_aor) ? 1 : 2;
@@ -4560,6 +4565,7 @@ bool OptionsDialog::TransferDataFromWindow()
     config->DeleteEntry(RegistrarNameKey);
     config->DeleteEntry(RegistrarUsernameKey);
     config->DeleteEntry(RegistrarPasswordKey);
+    config->DeleteEntry(RegistrarProxyKey);
     config->DeleteGroup(RegistrarGroup);
 
     int registrationIndex = 1;
@@ -5691,6 +5697,7 @@ BEGIN_EVENT_TABLE(RegistrationDialog, wxDialog)
   EVT_TEXT(wxXmlResource::GetXRCID(RegistrarContactKey), RegistrationDialog::Changed)
   EVT_TEXT(wxXmlResource::GetXRCID(RegistrarAuthIDKey), RegistrationDialog::Changed)
   EVT_TEXT(wxXmlResource::GetXRCID(RegistrarPasswordKey), RegistrationDialog::Changed)
+  EVT_TEXT(wxXmlResource::GetXRCID(RegistrarProxyKey), RegistrationDialog::Changed)
   EVT_TEXT(wxXmlResource::GetXRCID(RegistrarTimeToLiveKey), RegistrationDialog::Changed)
   EVT_BUTTON(wxXmlResource::GetXRCID(RegistrarUsedKey), RegistrationDialog::Changed)
 END_EVENT_TABLE()
@@ -5715,6 +5722,7 @@ RegistrationDialog::RegistrationDialog(wxDialog * parent, const RegistrationInfo
   FindWindowByNameAs<wxTextCtrl>(this, RegistrarContactKey      )->SetValidator(wxGenericValidator(&m_info.m_Contact));
   FindWindowByNameAs<wxTextCtrl>(this, RegistrarAuthIDKey       )->SetValidator(wxGenericValidator(&m_info.m_AuthID));
   FindWindowByNameAs<wxTextCtrl>(this, RegistrarPasswordKey     )->SetValidator(wxGenericValidator(&m_info.m_Password));
+  FindWindowByNameAs<wxTextCtrl>(this, RegistrarProxyKey        )->SetValidator(wxGenericValidator(&m_info.m_Proxy));
   FindWindowByNameAs<wxSpinCtrl>(this, RegistrarTimeToLiveKey   )->SetValidator(wxGenericValidator(&m_info.m_TimeToLive));
   FindWindowByNameAs<wxChoice  >(this, RegistrarCompatibilityKey)->SetValidator(wxGenericValidator((int *)&m_info.m_Compatibility));
 }
