1a9cc48a97a03c3e5b991414b55162a31eae0eb2
Robert Jongbloed <robertj@voxlucida.com.au>
2011-05-31 07:12:13 +0000

    Fixed local RTP detection code not handling CANCELed calls, leaves a dangling pointer to a deleted connection.

--- src/opal/rtpep.cxx
+++ src/opal/rtpep.cxx
@@ -182,8 +182,14 @@ void OpalRTPEndPoint::SetConnectionByRtpLocalPort(OpalRTPSession * rtp, OpalConn
 
   WORD localPort = rtp->GetLocalDataPort();
   inUseFlag.Wait();
-  if (connection == NULL)
-    m_connectionsByRtpLocalPort.erase(localPort);
+  if (connection == NULL) {
+    LocalRtpInfoMap::iterator it = m_connectionsByRtpLocalPort.find(localPort);
+    if (it != m_connectionsByRtpLocalPort.end()) {
+      PTRACE(4, "RTPEp\tSession " << rtp->GetSessionID() << ", "
+                "forgetting local RTP port " << localPort << " on connection " << it->second.m_connection);
+      m_connectionsByRtpLocalPort.erase(it);
+    }
+  }
   else {
     std::pair<LocalRtpInfoMap::iterator, bool> insertResult =
                 m_connectionsByRtpLocalPort.insert(LocalRtpInfoMap::value_type(localPort, *connection));
