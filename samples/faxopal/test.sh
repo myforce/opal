aefeb618d4c26e877999b8f46ed65d5a02aa61de
Robert Jongbloed <robertj@voxlucida.com.au>
2011-06-30 03:43:04 +0000

    Utilise new OpalMediaFormatList::Remove() functionality to assure that only G.711 and fax codecs are used.
    Improved script for testing fax modes.
    
    Fixed a log message indicating start of T.38 switch timer.
    
    git-svn-id: svn://svn.code.sf.net/p/opalvoip/code/opal/branches/v3_10@26098 023b2edf-31b2-4de3-b41e-bca80c47788f

--- samples/faxopal/test.sh
+++ samples/faxopal/test.sh
@@ -9,7 +9,9 @@ if [ -z "$FAXOPAL" ]; then
 fi
 
 RESULT_DIR=$CURDIR/results
-if [ ! -d $RESULT_DIR ]; then
+if [ -d $RESULT_DIR ]; then
+  rm -f $RESULT_DIR/*
+else
   mkdir $RESULT_DIR || exit $?
 fi
 
@@ -20,10 +22,30 @@ fi
 
 function test_fax()
 {
-  echo -n "Performing test $1 ... "
-  $FAXOPAL $4 -ttttto$RESULT_DIR/${1}_rx.log --sip udp$\*:15060 --h323 tcp$\*:11720 --timeout 2:30 $RESULT_DIR/${1}_rx.tif 2>&1 > $RESULT_DIR/${1}_rx.out &
+  echo -n "Performing test $1 $2 -> $3 ... "
+
+  if [ "$1" = "sip" ]; then
+    DEST_URL="sip:$HOST:15060"
+  else
+    DEST_URL="h323:$HOST:11720"
+  fi
+
+  if [ "$2" = "g711" ]; then
+    TX_ARG="-a"
+  fi
+
+  if [ "$3" = "g711" ]; then
+    RX_ARG="-a"
+  fi
+
+  if [ "$4" = "slow" ]; then
+    TX_ARG="-F $TX_ARG"
+    RX_ARG="-F $RX_ARG"
+  fi
+
+  $FAXOPAL $RX_ARG -ttttto$RESULT_DIR/${1}_rx.log --sip udp$\*:15060 --h323 tcp$\*:11720 --timeout 2:30 $RESULT_DIR/${1}_rx.tif 2>&1 > $RESULT_DIR/${1}_rx.out &
   sleep 2
-  $FAXOPAL $3 -ttttto$RESULT_DIR/${1}_tx.log --sip udp$\*:25060 --h323 tcp$\*:21720 $CURDIR/F06_200.tif $2 2>&1 > $RESULT_DIR/${1}_tx.out &
+  $FAXOPAL $TX_ARG -ttttto$RESULT_DIR/${1}_tx.log --sip udp$\*:25060 --h323 tcp$\*:21720 $CURDIR/F06_200.tif $DEST_URL 2>&1 > $RESULT_DIR/${1}_tx.out &
   wait
   if grep -q success $RESULT_DIR/${1}_rx.out && grep -q success $RESULT_DIR/${1}_tx.out; then
     echo "successful."
@@ -32,18 +54,24 @@ function test_fax()
   fi
 }
 
-test_fax sip_t38_t38   "sip:$HOST:15060" ""   ""
-test_fax sip_g711_t38  "sip:$HOST:15060" "-a" ""
-test_fax sip_t38_g711  "sip:$HOST:15060" ""   "-a"
-test_fax sip_g711_g711 "sip:$HOST:15060" "-a" "-a"
+if [ $# -ge 3 ]; then
+  test_fax $*
+elif [ $# = 0 ]; then
+  test_fax sip t38  t38
+  test_fax sip g711 t38
+  test_fax sip t38  g711
+  test_fax sip g711 g711
 
-test_fax h323_fast_t38_t38   "h323:$HOST:11720" ""   ""
-test_fax h323_fast_g711_t38  "h323:$HOST:11720" "-a" ""
-test_fax h323_fast_t38_g711  "h323:$HOST:11720" ""   "-a"
-test_fax h323_fast_g711_g711 "h323:$HOST:11720" "-a" "-a"
+  test_fax h323 t38  t38  fast
+  test_fax h323 g711 t38  fast
+  test_fax h323 t38  g711 fast
+  test_fax h323 g711 g711 fast
 
-test_fax h323_slow_t38_t38   "h323:$HOST:11720" "-F"    "-F"
-test_fax h323_slow_g711_t38  "h323:$HOST:11720" "-F -a" "-F"
-test_fax h323_slow_t38_g711  "h323:$HOST:11720" "-F"    "-F -a"
-test_fax h323_slow_g711_g711 "h323:$HOST:11720" "-F -a" "-F -a"
+  test_fax h323 t38  t38  slow
+  test_fax h323 g711 t38  slow
+  test_fax h323 t38  g711 slow
+  test_fax h323 g711 g711 slow
+else
+  echo "usage: $0 { sip | h323 } { t38 | g711 } { t38 | g711 } [ slow | fast ]"
+fi
 
