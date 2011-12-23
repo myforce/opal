64ebb897001fb19d51e9e2f653a7a44d86ea8f0d
Robert Jongbloed <robertj@voxlucida.com.au>
2011-12-23 02:30:00 +0000

    Make sure application gets transfer failed indication if the REFER gets a non 2xx response.

--- src/sip/sipcon.cxx
+++ src/sip/sipcon.cxx
@@ -2048,6 +2048,15 @@ void SIPConnection::OnReceivedResponse(SIPTransaction & transaction, SIP_PDU & r
           case 2 : // Successful response - there really is only 200 OK
             OnReceivedOK(transaction, response);
             break;
+
+          default :
+            if (transaction.GetMethod() == SIP_PDU::Method_REFER) {
+              PStringToString info;
+              info.SetAt("result", "error");
+              info.SetAt("party", "A");
+              info.SetAt("code", psprintf("%u", response.GetStatusCode()));
+              OnTransferNotify(info, this);
+            }
         }
     }
 
@@ -2096,6 +2105,7 @@ void SIPConnection::OnReceivedResponse(SIPTransaction & transaction, SIP_PDU & r
           PStringToString info;
           info.SetAt("result", "failed");
           info.SetAt("party", "A");
+          info.SetAt("code", psprintf("%u", response.GetStatusCode()));
           OnTransferNotify(info, this);
         }
         else if (response.GetStatusCode() >= 200) {
