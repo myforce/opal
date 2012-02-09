017ab5951cd0fb33d48ec99b7ec21894c252d2d0
Robert Jongbloed <robertj@voxlucida.com.au>
2012-02-09 02:25:28 +0000

    Attempt to fix deadlock when get a combination of timeouts & fatal error responses in REGISTER/SUBSCRIBE operations.
    git-svn-id: svn://svn.code.sf.net/p/opalvoip/code/opal/branches/v3_10@26961 023b2edf-31b2-4de3-b41e-bca80c47788f

--- src/sip/handlers.cxx
+++ src/sip/handlers.cxx
@@ -132,10 +132,10 @@ bool SIPHandler::ShutDown()
   {
     PSafeLockReadWrite mutex(*this);
     if (!mutex.IsLocked())
-      return true;
+    return true;
 
-    while (!m_stateQueue.empty())
-      m_stateQueue.pop();
+  while (!m_stateQueue.empty())
+    m_stateQueue.pop();
 
     switch (GetState()) {
       case Subscribed :
@@ -190,6 +190,9 @@ void SIPHandler::SetState(SIPHandler::State newState)
 
 bool SIPHandler::ActivateState(SIPHandler::State newState)
 {
+  if (GetState() == Unsubscribed)
+    return false;
+
   // If subscribing with zero expiry time, is same as unsubscribe
   if (newState == Subscribing && GetExpire() == 0)
     newState = Unsubscribing;
@@ -197,7 +200,7 @@ bool SIPHandler::ActivateState(SIPHandler::State newState)
   // If unsubscribing and never got a response from server, rather than trying
   // to send more unsuccessful packets, abort transactions and mark Unsubscribed
   if (newState == Unsubscribing && !m_receivedResponse) {
-    ShutDown();
+    SetState(Unsubscribed); // Allow garbage collection thread to clean up
     return true;
   }
 
@@ -596,8 +599,7 @@ void SIPHandler::OnFailed(SIP_PDU::StatusCodes code)
       PTRACE(4, "SIP\tNot retrying " << GetMethod() << " due to error response " << code);
       m_currentExpireTime = 0; // OK, stop trying
       m_expireTimer.Stop(false);
-      SetState(Unsubscribed);
-      ShutDown();
+      SetState(Unsubscribed); // Allow garbage collection thread to clean up
   }
 }
 
@@ -1097,12 +1099,22 @@ PBoolean SIPSubscribeHandler::OnReceivedNOTIFY(SIP_PDU & request)
     PTRACE(3, "SIP\tSubscription is terminated, state=" << GetState());
     m_previousResponse->SetStatusCode(SIP_PDU::Successful_OK);
     request.SendResponse(*m_transport, *m_previousResponse, &endpoint);
-    if (GetState() != Unsubscribing)
-      ShutDown();
-    else {
-      SetState(Unsubscribed);
-      SendStatus(SIP_PDU::Successful_OK, Unsubscribing);
+
+    switch (GetState()) {
+      case Unsubscribed :
+        break;
+
+      case Subscribed :
+      case Unavailable :
+        SendRequest(Unsubscribing);
+        break;
+
+      default :
+        SetState(Unsubscribed); // Allow garbage collection thread to clean up
+        SendStatus(SIP_PDU::Successful_OK, Unsubscribing);
+        break;
     }
+
     return true;
   }
 
