3215dc40521df55ee4a56680277f34e47bb86fd0
Robert Jongbloed <robertj@voxlucida.com.au>
2012-05-01 06:59:05 +0000

    Make sure is locally compiled SRTP is compatible with stdint.h being included.
    git-svn-id: svn://svn.code.sf.net/p/opalvoip/code/opal/trunk@27551 023b2edf-31b2-4de3-b41e-bca80c47788f

--- src/rtp/libsrtp/win32/config.h
+++ src/rtp/libsrtp/win32/config.h
@@ -175,6 +175,7 @@
 #pragma warning(disable:LIBSRTP_DISABLED_MSVC_WARNINGS)
 #define PACK_STRUCTURE __declspec(align(1))
 
+#ifndef _STDINT
 typedef char    int8_t;
 typedef short   int16_t;
 typedef int     int32_t;
@@ -184,3 +185,4 @@ typedef unsigned char    uint8_t;
 typedef unsigned short   uint16_t;
 typedef unsigned int     uint32_t;
 typedef unsigned __int64 uint64_t;
+#endif
