6100058dd82b48d6a9fa3bfb86f306566b15d852
Robert Jongbloed <robertj@voxlucida.com.au>
2012-05-09 00:36:51 +0000

    Make sure OpalCall party names updated after SIP transfer.

--- src/sip/sipcon.cxx
+++ src/sip/sipcon.cxx
@@ -2210,6 +2210,8 @@ void SIPConnection::UpdateRemoteAddresses()
   // If no local name, then use what the remote thinks we are
   if (localPartyName.IsEmpty())
     localPartyName = m_dialog.GetLocalURI().GetUserName();
+
+  ownerCall.SetPartyNames();
 }
 
 
@@ -2753,7 +2755,9 @@ void SIPConnection::OnReceivedReINVITE(SIP_PDU & request)
   m_answerFormatList.RemoveAll();
 
   SIPURL newRemotePartyID(request.GetMIME(), RemotePartyID);
-  if (!newRemotePartyID.IsEmpty() && m_ciscoRemotePartyID != newRemotePartyID) {
+  if (newRemotePartyID.IsEmpty() || m_ciscoRemotePartyID == newRemotePartyID)
+    UpdateRemoteAddresses();
+  else {
     PTRACE(3, "SIP\tOld style Remote-Party-ID used for transfer indication to \"" << newRemotePartyID << '"');
 
     m_ciscoRemotePartyID = newRemotePartyID;
