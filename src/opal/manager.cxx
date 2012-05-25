d262041729dd0423373edd0c8c3d36434494fcd1
Robert Jongbloed <robertj@voxlucida.com.au>
2012-05-25 03:46:05 +0000

    Fixed last RTP timestamp being sent in RTCP sender reports, thanks Denis Loh.
    Applied patch ID: 3529417 "Calculate RTP timestamp for SenderReports", thanks Denis Loh
    
    Fixed GNU warning, thanks Denis Loh
    
    Fixed strange crash in H264 encoder caused by a compiler optimisation flag.
    
    Added PTRACE of every OpenPhone LogWindow message.

--- src/opal/manager.cxx
+++ src/opal/manager.cxx
@@ -1139,7 +1139,8 @@ PBoolean OpalManager::CreateVideoOutputDevice(const OpalConnection & connection,
   PINDEX start = args.deviceName.Find("TITLE=\"");
   if (start != P_MAX_INDEX) {
     start += 7;
-    args.deviceName.Splice(preview ? "Local Preview" : connection.GetRemotePartyName(), start, args.deviceName.Find('"', start)-start);
+    static PConstString const LocalPreview("Local Preview");
+    args.deviceName.Splice(preview ? LocalPreview : connection.GetRemotePartyName(), start, args.deviceName.Find('"', start)-start);
   }
 
   autoDelete = true;
