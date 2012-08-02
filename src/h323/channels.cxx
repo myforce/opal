afc796922555e2f228363f2f843bdce1bbf052da
Robert Jongbloed <robertj@voxlucida.com.au>
2012-08-02 01:36:06 +0000

    Fixed H.323 bandwidth control on individual channel, an audio transmitter had a zero bps used due to not having a "target" bit rate.
    git-svn-id: svn://svn.code.sf.net/p/opalvoip/code/opal/branches/v3_10@28137 023b2edf-31b2-4de3-b41e-bca80c47788f

--- src/h323/channels.cxx
+++ src/h323/channels.cxx
@@ -363,9 +363,12 @@ H323Channel::Directions H323UnidirectionalChannel::GetDirection() const
 
 PBoolean H323UnidirectionalChannel::SetInitialBandwidth()
 {
-  return SetBandwidthUsed(capability->GetMediaFormat().GetOptionInteger(
-                          GetDirection() == IsReceiver ? OpalMediaFormat::MaxBitRateOption()
-                                                       : OpalMediaFormat::TargetBitRateOption())/100);
+  unsigned bandwidth = 0;
+  if (GetDirection() == IsTransmit)
+    bandwidth = capability->GetMediaFormat().GetOptionInteger(OpalMediaFormat::TargetBitRateOption());
+  if (bandwidth == 0)
+    bandwidth = capability->GetMediaFormat().GetOptionInteger(OpalMediaFormat::MaxBitRateOption());
+  return SetBandwidthUsed(bandwidth/100);
 }
 
 
