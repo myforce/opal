39f7dce665db41d2481d7808b60fe3a2941eb617
Eugen Dedu <ededu@src.gnome.org>
2012-08-20 14:27:18 +0000

    Fix libavformat.h inclusion
    git-svn-id: svn://svn.code.sf.net/p/opalvoip/code/opal/branches/v3_10@28216 023b2edf-31b2-4de3-b41e-bca80c47788f

--- plugins/video/common/ffmpeg.h
+++ plugins/video/common/ffmpeg.h
@@ -46,7 +46,10 @@
 #include "platform.h"
 
 #include "libavcodec/avcodec.h"
+// AVPacket was declared in avformat.h before April 2009
+#if LIBAVCODEC_VERSION_INT <= AV_VERSION_INT(52, 25, 0)
 #include "libavformat/avformat.h"
+#endif
 
 #ifndef LIBAVCODEC_VERSION_INT
 #error Libavcodec include is not correct
