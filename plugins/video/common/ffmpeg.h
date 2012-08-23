2bca3e10c1bf1cf67d52656b2351ee8a25ba3656
Robert Jongbloed <robertj@voxlucida.com.au>
2012-08-23 05:20:36 +0000

    Merged revision(s) 28182-28243 from opal/branches/v3_10: Remove obsolete check ........

--- plugins/video/common/ffmpeg.h
+++ plugins/video/common/ffmpeg.h
@@ -46,6 +46,10 @@
 #include "dyna.h"
 
 #include "libavcodec/avcodec.h"
+// AVPacket was declared in avformat.h before April 2009
+#if LIBAVCODEC_VERSION_INT <= AV_VERSION_INT(52, 25, 0)
+#include "libavformat/avformat.h"
+#endif
 
 // AVPacket was declared in avformat.h before April 2009
 #if LIBAVCODEC_VERSION_INT <= AV_VERSION_INT(52, 25, 0)
