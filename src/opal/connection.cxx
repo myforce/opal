d8874b12a712c7196f013e3b453523950d4447ab
Robert Jongbloed <robertj@voxlucida.com.au>
2012-08-30 02:54:50 +0000

    Added PSoundChannel::SetMute/GetMute functions, thanks Dmitry

--- src/opal/connection.cxx
+++ src/opal/connection.cxx
@@ -1265,6 +1265,18 @@ PBoolean OpalConnection::GetAudioVolume(PBoolean /*source*/, unsigned & /*percen
 }
 
 
+bool OpalConnection::SetAudioMute(bool /*source*/, bool /*mute*/)
+{
+  return false;
+}
+
+
+bool OpalConnection::GetAudioMute(bool /*source*/, bool & /*mute*/)
+{
+  return false;
+}
+
+
 unsigned OpalConnection::GetAudioSignalLevel(PBoolean /*source*/)
 {
     return UINT_MAX;
