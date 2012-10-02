300d02e65bfacb8dfe88609f546cc0a882369031
Robert Jongbloed <robertj@voxlucida.com.au>
2012-10-02 08:44:11 +0000

    Back ported fax test script from Eridani
    git-svn-id: svn://svn.code.sf.net/p/opalvoip/code/opal/branches/v3_10@28442 023b2edf-31b2-4de3-b41e-bca80c47788f

--- samples/faxopal/test.sh
+++ samples/faxopal/test.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+#set -x
+
 CURDIR=`pwd`
 
 FAXOPAL=`ls $CURDIR/obj_*/faxopal | head -1`
@@ -15,7 +17,10 @@ else
   mkdir $RESULT_DIR || exit $?
 fi
 
-HOST=`/sbin/ifconfig eth0 2>/dev/null | grep 'inet addr' | sed -e 's/ *inet addr://' -e 's/ *Bcast.*$//'`
+IFACE=`/sbin/ifconfig | head -1 | cut -d ' ' -f 1`
+if [ -n "$IFACE" ]; then
+  HOST=`/sbin/ifconfig $IFACE 2>/dev/null | grep 'inet addr' | sed -e 's/ *inet addr://' -e 's/ *Bcast.*$//'`
+fi
 if [ -z "$HOST" ]; then
   HOST=`hostname`
 fi
@@ -24,30 +29,62 @@ function test_fax()
 {
   echo -n "Performing test $1 $2 -> $3 ... "
 
+  RESULT_PREFIX="$RESULT_DIR/${1}_${2}_${3}_"
+  if [ -n "$4" ]; then
+    RESULT_PREFIX+="${4}_"
+  fi
+
+  TX_ARG="-o${RESULT_PREFIX}tx.log"
+  RX_ARG="-o${RESULT_PREFIX}rx.log"
+  XX_ARG="-ttttt --no-lid --no-capi --timeout 2:30"
+
   if [ "$1" = "sip" ]; then
+    TX_ARG+=" --sip udp\$$HOST:25060"
+    RX_ARG+=" --sip udp\$$HOST:15060"
+    XX_ARG+=" --no-h323"
     DEST_URL="sip:$HOST:15060"
   else
+    TX_ARG+=" --h323 tcp\$$HOST:21720"
+    RX_ARG+=" --h323 tcp\$$HOST:11720"
+    XX_ARG+=" --no-sip"
     DEST_URL="h323:$HOST:11720"
   fi
 
   if [ "$2" = "g711" ]; then
-    TX_ARG="-a"
+    TX_ARG+=" --audio"
   fi
 
   if [ "$3" = "g711" ]; then
-    RX_ARG="-a"
+    RX_ARG+=" --audio"
   fi
 
   if [ "$4" = "slow" ]; then
-    TX_ARG="-F $TX_ARG"
-    RX_ARG="-F $RX_ARG"
+    XX_ARG+=" --no-fast"
   fi
 
-  $FAXOPAL $RX_ARG -ttttto$RESULT_DIR/${1}_rx.log --sip udp$\*:15060 --h323 tcp$\*:11720 --timeout 2:30 $RESULT_DIR/${1}_rx.tif 2>&1 > $RESULT_DIR/${1}_rx.out &
+  if [ "$2" = "t38" -a "$3" = "g711" ]; then
+    TX_ARG+=" --switch-on-ced"
+  fi
+
+  if [ "$2" = "t38" -a "$3" = "t38" ]; then
+    XX_ARG+=" --no-fallback"
+  fi
+
+  TX_ARG+=" $CURDIR/F06_200.tif $DEST_URL"
+  RX_ARG+=" ${RESULT_PREFIX}rx.tif"
+
+  TX_OUT=${RESULT_PREFIX}tx.out
+  RX_OUT=${RESULT_PREFIX}rx.out
+
+  echo $FAXOPAL $XX_ARG $TX_ARG > $TX_OUT
+  echo $FAXOPAL $XX_ARG $RX_ARG > $RX_OUT
+
+  $FAXOPAL $XX_ARG $RX_ARG < /dev/null >> $RX_OUT 2>&1 &
   sleep 2
-  $FAXOPAL $TX_ARG -ttttto$RESULT_DIR/${1}_tx.log --sip udp$\*:25060 --h323 tcp$\*:21720 $CURDIR/F06_200.tif $DEST_URL 2>&1 > $RESULT_DIR/${1}_tx.out &
+  $FAXOPAL $XX_ARG $TX_ARG < /dev/null >> $TX_OUT 2>&1 &
   wait
-  if grep -q success $RESULT_DIR/${1}_rx.out && grep -q success $RESULT_DIR/${1}_tx.out; then
+  wait
+  if grep -q Success $TX_OUT && grep -q Success $RX_OUT ; then
     echo "successful."
   else
     echo "failed."
