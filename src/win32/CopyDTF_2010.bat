22bcc3b9abdceae6a228fdcf8f804a068cdece46
Robert Jongbloed <robertj@voxlucida.com.au>
2012-10-04 12:46:32 +0000

    Update symbols. Remove VS2005 symbols, no longer supported.
    git-svn-id: svn://svn.code.sf.net/p/opalvoip/code/opal/trunk@28462 023b2edf-31b2-4de3-b41e-bca80c47788f

--- src/win32/CopyDTF_2010.bat
+++ src/win32/CopyDTF_2010.bat
@@ -1,24 +1,17 @@
 mkdir backup
 
 move opald_2010.dtf backup\
-copy ..\..\lib\debug\opald.def opald_2010.dtf
+copy ..\..\lib\Win32\debug\opald.def opald_2010.dtf
 move opal_2010.dtf backup\
-copy ..\..\lib\release\opal.def opal_2010.dtf
+copy ..\..\lib\Win32\release\opal.def opal_2010.dtf
 move opaln_2010.dtf backup\
-copy "..\..\lib\no trace\opaln.def" opaln_2010.dtf
+copy "..\..\lib\Win32\No Trace\opaln.def" opaln_2010.dtf
 
-rem move opald_2010_wm.dtf backup\
-rem copy ..\..\lib\wm5ppc\debug\opald.def opald_2010_wm.dtf
-rem move opal_2010_wm.dtf backup\
-rem copy ..\..\lib\wm5ppc\release\opal.def opal_2010_wm.dtf
-rem move opaln_2010_wm.dtf backup\
-rem copy "..\..\lib\wm5ppc\no trace\opaln.def" opaln_2010_wm.dtf
-
-rem move opald_2010_wm6.dtf backup\
-rem copy ..\..\lib\wm6pro\debug\opald.def opald_2010_wm6.dtf
-rem move opal_2010_wm6.dtf backup\
-rem copy ..\..\lib\wm6pro\release\opal.def opal_2010_wm6.dtf
-rem move opaln_2010_wm6.dtf backup\
-rem copy "..\..\lib\wm6pro\no trace\opaln.def" opaln_2010_wm6.dtf
+move opald_2010_x64.dtf backup\
+copy ..\..\lib\x64\debug\opal64d.def opald_2010_x64.dtf
+move opal_2010_x64.dtf backup\
+copy ..\..\lib\x64\release\opal64.def opal_2010_x64.dtf
+move opaln_2010_x64.dtf backup\
+copy "..\..\lib\x64\no trace\opal64n.def" opaln_2010_x64.dtf
 
 pause
