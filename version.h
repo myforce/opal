d65c5cefa793b54fdcdc55481796e46a6b20b409
Robert Jongbloed <robertj@voxlucida.com.au>
2012-10-05 06:20:40 +0000

    Update version number for beta v3.10.9
    git-svn-id: svn://svn.code.sf.net/p/opalvoip/code/opal/branches/v3_10@28477 023b2edf-31b2-4de3-b41e-bca80c47788f

--- version.h
+++ version.h
@@ -36,8 +36,8 @@
 
 #define MAJOR_VERSION 3
 #define MINOR_VERSION 10
-#define BUILD_TYPE ReleaseCode
-#define BUILD_NUMBER 8
+#define BUILD_TYPE BetaCode
+#define BUILD_NUMBER 9
 
 
 #endif  // _OPAL_VERSION_H
