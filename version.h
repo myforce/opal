be973e62cd9e136bbe2bc545990b4f66e74d003d
Robert Jongbloed <robertj@voxlucida.com.au>
2012-10-05 09:53:34 +0000

    Update version number for alpha v3.13.0

--- version.h
+++ version.h
@@ -35,9 +35,9 @@
  */
 
 #define MAJOR_VERSION 3
-#define MINOR_VERSION 11
+#define MINOR_VERSION 13
 #define BUILD_TYPE AlphaCode
-#define BUILD_NUMBER 2
+#define BUILD_NUMBER 0
 
 
 #endif  // _OPAL_VERSION_H
