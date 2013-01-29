d2198fd0a334dc0c93c44e5af4616c358e7e67d1
Robert Jongbloed <robertj@voxlucida.com.au>
2013-01-29 23:37:53 +0000

    Fixed endlessly creating OPTIONS handlers, especially with NAT refresh, and never disposing of them.
    git-svn-id: svn://svn.code.sf.net/p/opalvoip/code/opal/branches/v3_10@29036 023b2edf-31b2-4de3-b41e-bca80c47788f

--- src/sip/sipep.cxx
+++ src/sip/sipep.cxx
@@ -1508,7 +1508,7 @@ bool SIPEndPoint::SendOPTIONS(const SIPOptions::Params & newParams)
 
   PSafePtr<SIPHandler> handler = new SIPOptionsHandler(*this, params);
   activeSIPHandlers.Append(handler);
-  return handler->ActivateState(SIPHandler::Subscribing);
+  return handler->ActivateState(SIPHandler::Unsubscribing);
 }
 
 
