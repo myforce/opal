5417296726aed3e0550fa92ab9b74bafe4482c6e
Robert Jongbloed <robertj@voxlucida.com.au>
2013-03-12 08:55:56 +0000

    Fixeed GNU warning
    git-svn-id: svn://svn.code.sf.net/p/opalvoip/code/opal/trunk@29251 023b2edf-31b2-4de3-b41e-bca80c47788f

--- src/sip/sdp.cxx
+++ src/sip/sdp.cxx
@@ -1198,7 +1198,7 @@ PCaselessString SDPRTPAVPMediaDescription::GetSDPTransportType() const
 
 bool SDPRTPAVPMediaDescription::Format::Initialise(const PString & portString)
 {
-  unsigned pt = portString.AsInteger();
+  int pt = portString.AsInteger();
   if (pt < 0 || pt > 127)
     return false;
 
