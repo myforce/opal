d1f5277c78b39c63237180409c3f4a76699ed5be
Robert Jongbloed <robertj@voxlucida.com.au>
2013-03-28 00:06:48 +0000

    Update symbols
    git-svn-id: svn://svn.code.sf.net/p/opalvoip/code/opal/branches/v3_12@29353 023b2edf-31b2-4de3-b41e-bca80c47788f

--- src/win32/CopyDTF_2010.bat
+++ src/win32/CopyDTF_2010.bat
@@ -1,17 +1,9 @@
-mkdir backup
-
-move opald_2010.dtf backup\
 copy ..\..\lib\Win32\debug\opald.def opald_2010.dtf
-move opal_2010.dtf backup\
 copy ..\..\lib\Win32\release\opal.def opal_2010.dtf
-move opaln_2010.dtf backup\
 copy "..\..\lib\Win32\No Trace\opaln.def" opaln_2010.dtf
 
-move opald_2010_x64.dtf backup\
 copy ..\..\lib\x64\debug\opal64d.def opald_2010_x64.dtf
-move opal_2010_x64.dtf backup\
 copy ..\..\lib\x64\release\opal64.def opal_2010_x64.dtf
-move opaln_2010_x64.dtf backup\
 copy "..\..\lib\x64\no trace\opal64n.def" opaln_2010_x64.dtf
 
 pause
