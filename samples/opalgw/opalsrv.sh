59e923f849b5672cf84efe11fc46513db6b67d8e
Robert Jongbloed <robertj@voxlucida.com.au>
2013-03-30 07:02:30 +0000

    Merged revision(s) 29378-29395 from opal/branches/v3_12: --------------------- r29394 More help on configuratio page for opalgw. Added script to start and stop daemon in non-root environment. --------------------- r29393 Fixed some issues with OPAL garbage collection thread. Don't start in constructor as if then used in service process in daeomon mode, the thread is not really started, but the code thinks it is. Also fixed a very small race condition on add... --------------------- r29383 Changes to OpenPhone installer, using configure.exe enhancement, so get version number automatically from OPAL.

---
+++ samples/opalgw/opalsrv.sh
@@ -0,0 +1,38 @@
+#!/bin/sh
+
+case $1 in
+  start )
+    OP="--daemon"
+  ;;
+
+  stop )
+    OP="--kill"
+  ;;
+
+  status )
+    OP="--status"
+  ;;
+
+  *)
+    echo "usage: $0 start | stop | status"
+    exit 1
+  ;;
+esac
+
+NAME=opalgw
+DIR=$HOME/.opalsrv
+EXE=`ls ./obj*[^d]/$NAME`
+
+if [ ! -x $EXE ]; then
+  echo "No executable found"
+  exit 1
+fi
+
+if [ ! -d $DIR ]; then
+  mkdir $DIR || exit $?
+fi
+
+$EXE --pid-file $DIR/${NAME}.pid \
+     --ini-file $DIR/${NAME}.ini \
+     --log-file $DIR/${NAME}.log \
+     $OP
