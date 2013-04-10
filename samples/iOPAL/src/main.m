05d441f407289e97ed958721bcded28a6a5c0539
Robert Jongbloed <robertj@voxlucida.com.au>
2013-04-10 07:14:33 +0000

    Added iOS (XCode) sample application.

---
+++ samples/iOPAL/src/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  iOPAL
+//
+//  Created by Robert Jongbloed on 4/04/13.
+//  Copyright (c) 2013 Vox Lucida Pty. Ltd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "iOpalAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+  @autoreleasepool {
+      return UIApplicationMain(argc, argv, nil, NSStringFromClass([iOpalAppDelegate class]));
+  }
+}
