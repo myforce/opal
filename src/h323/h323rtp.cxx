35615b7bf37acab47e20d829545ce456b3825314
Robert Jongbloed <robertj@voxlucida.com.au>
2013-04-11 02:05:01 +0000

    Fixed merge error
    git-svn-id: svn://svn.code.sf.net/p/opalvoip/code/opal/branches/v3_12@29487 023b2edf-31b2-4de3-b41e-bca80c47788f

--- src/h323/h323rtp.cxx
+++ src/h323/h323rtp.cxx
@@ -222,7 +222,7 @@ PBoolean H323_RTPChannel::OnReceivedPDU(const H245_H2250LogicalChannelParameters
     mediaPacketization = H323GetRTPPacketization(param.m_mediaPacketization);
 
   // Hack for H.263 compatibility, default to RFC2190 if not told otherwise
-  if (mediaPacketization.IsEmpty() && mediaFormat == OPAL_H263)
+  if (mediaPacketization.IsEmpty() && m_mediaFormat == OPAL_H263)
     mediaPacketization = "RFC2190";
 
   m_mediaFormat.SetPayloadType(rtpPayloadType);
