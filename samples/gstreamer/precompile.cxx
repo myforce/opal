d421e950265cda5d70a0012fb01e1f3c077a39fa
Robert Jongbloed <robertj@voxlucida.com.au>
2013-04-15 07:58:17 +0000

    Implemented audio on GStream endpoint. Added test application for GStreamer.
    git-svn-id: svn://svn.code.sf.net/p/opalvoip/code/opal/trunk@29505 023b2edf-31b2-4de3-b41e-bca80c47788f

---
+++ samples/gstreamer/precompile.cxx
@@ -0,0 +1,32 @@
+/*
+ * precompile.cxx
+ *
+ * OPAL application source file for GStreamer OPAL sample
+ *
+ * Copyright (c) 2013 Vox Lucida Pty. Ltd.
+ *
+ * The contents of this file are subject to the Mozilla Public License
+ * Version 1.0 (the "License"); you may not use this file except in
+ * compliance with the License. You may obtain a copy of the License at
+ * http://www.mozilla.org/MPL/
+ *
+ * Software distributed under the License is distributed on an "AS IS"
+ * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See
+ * the License for the specific language governing rights and limitations
+ * under the License.
+ *
+ * The Original Code is Open Phone Abstraction Library.
+ *
+ * The Initial Developer of the Original Code is Equivalence Pty. Ltd.
+ *
+ * Contributor(s): ______________________________________.
+ *
+ * $Revision$
+ * $Author$
+ * $Date$
+ */
+
+#include "precompile.h"
+
+
+// End of File ///////////////////////////////////////////////////////////////
---
+++ samples/gstreamer/precompile.h
@@ -0,0 +1,36 @@
+/*
+ * precompile.h
+ *
+ * OPAL application source file for GStreamer OPAL sample
+ *
+ * Copyright (c) 2013 Vox Lucida Pty. Ltd.
+ *
+ * The contents of this file are subject to the Mozilla Public License
+ * Version 1.0 (the "License"); you may not use this file except in
+ * compliance with the License. You may obtain a copy of the License at
+ * http://www.mozilla.org/MPL/
+ *
+ * Software distributed under the License is distributed on an "AS IS"
+ * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See
+ * the License for the specific language governing rights and limitations
+ * under the License.
+ *
+ * The Original Code is Open Phone Abstraction Library.
+ *
+ * The Initial Developer of the Original Code is Equivalence Pty. Ltd.
+ *
+ * Contributor(s): ______________________________________.
+ *
+ * $Revision$
+ * $Author$
+ * $Date$
+ */
+
+#include <ptlib.h>
+#include <opal/console_mgr.h>
+#include <ep/GstEndPoint.h>
+#include <sip/sipep.h>
+#include <h323/h323ep.h>
+
+
+// End of File ///////////////////////////////////////////////////////////////
