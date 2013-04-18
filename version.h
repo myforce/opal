8f56940de9e7f9870492f792e6c46b4b54b3a028
Robert Jongbloed <robertj@voxlucida.com.au>
2013-04-18 05:14:10 +0000

    Update release version number to 3.13.1
    git-svn-id: svn://svn.code.sf.net/p/opalvoip/code/opal/trunk@29531 023b2edf-31b2-4de3-b41e-bca80c47788f

--- version.h
+++ version.h
@@ -36,8 +36,8 @@
 
 #define MAJOR_VERSION 3
 #define MINOR_VERSION 13
-#define BUILD_TYPE AlphaCode
-#define BUILD_NUMBER 0
+#define BUILD_TYPE ReleaseCode
+#define BUILD_NUMBER 1
 
 
 #endif  // _OPAL_VERSION_H
