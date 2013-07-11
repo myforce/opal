fdaff8642fdf3d9fd9e15f57f00138a6991e95a1
Robert Jongbloed <robertj@voxlucida.com.au>
2013-07-11 03:28:37 +0000

    Fixed being able to dial h323s URL
    git-svn-id: svn://svn.code.sf.net/p/opalvoip/code/opal/branches/v3_12@30161 023b2edf-31b2-4de3-b41e-bca80c47788f

--- src/h323/h323ep.cxx
+++ src/h323/h323ep.cxx
@@ -804,6 +804,10 @@ PBoolean H323EndPoint::ParsePartyName(const PString & remoteParty,
     /* URL default for H.323 is if no @ then is username, but that does
        not make sense when no GK, so we switch to being a host only */
     if (url.GetHostName().IsEmpty()) {
+      if (url.GetScheme() == "tel") {
+        PTRACE(2, "H323\tCannot use tel URI without phone-context or active gatekeeper");
+        return false;
+      }
       url.SetHostName(url.GetUserName());
       url.SetUserName(PString::Empty());
     }
@@ -821,10 +825,11 @@ PBoolean H323EndPoint::ParsePartyName(const PString & remoteParty,
 
 #endif // OPAL_PTLIB_DNS_RESOLVER
 
-  address = url.GetHostName();
-  if (!address && url.GetPort() != 0)
-    address.sprintf(":%u", url.GetPort());
-
+  address = OpalTransportAddress(url.GetHostName(), url.GetPort(),
+#if OPAL_PTLIB_SSL
+                                 url.GetScheme() == "h323s" ? OpalTransportAddress::TlsPrefix() :
+#endif
+                                 OpalTransportAddress::TcpPrefix());
   alias = url.GetUserName();
   if (alias.IsEmpty() && address.IsEmpty()) {
     PTRACE(1, "H323\tAttempt to use invalid URL \"" << remoteParty << '"');
