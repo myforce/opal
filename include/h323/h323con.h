115105b8c83f9b55e84400eb4a061b023129ace7
Robert Jongbloed <robertj@voxlucida.com.au>
2013-09-30 06:52:34 +0000

    Move initial call set up into background thread. When starting a SIP/H.323 call there are DNS lookups (for ENUM and SRV records) that can take some time, especially of the DNS server is offline. In SIP the initial INVITE is also sent in the context of the caller thread. While this is usually only a "second or two" delay, it can be disconcerting in GUI applications as the GUI is frozen for that time, and also for server applications it limits the rate calls can be started even though it is a candidate for parallel operation.
    git-svn-id: svn://svn.code.sf.net/p/opalvoip/code/opal/branches/v3_12@30586 023b2edf-31b2-4de3-b41e-bca80c47788f

--- include/h323/h323con.h
+++ include/h323/h323con.h
@@ -2147,7 +2147,6 @@ class H323Connection : public OpalRTPConnection
     void SetRemoteVersions(const H225_ProtocolIdentifier & id);
     void SetBearerCapabilities(H323SignalPDU & pdu) const;
     void MonitorCallStatus();
-    PDECLARE_NOTIFIER(PThread, H323Connection, StartOutgoing);
     PDECLARE_NOTIFIER(PThread, H323Connection, NewOutgoingControlChannel);
     PDECLARE_AcceptHandlerNotifier(H323Connection, NewIncomingControlChannel);
 
