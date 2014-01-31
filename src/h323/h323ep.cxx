ef1d217c77a29278bfe1ff3e468c83cbd7e267eb
Robert Jongbloed <robertj@voxlucida.com.au>
2014-01-31 08:17:55 +0000

    Fixed some weird compatibility issues with some systems around the on hold/off hold TCS media switcharoo thing.
    git-svn-id: svn://svn.code.sf.net/p/opalvoip/code/opal/trunk@31332 023b2edf-31b2-4de3-b41e-bca80c47788f

--- src/h323/h323ep.cxx
+++ src/h323/h323ep.cxx
@@ -126,7 +126,8 @@ H323EndPoint::H323EndPoint(OpalManager & manager)
   SetCompatibility(H323Connection::e_BadMasterSlaveConflict,  "NetMeeting");
   AddCompatibility(H323Connection::e_BadMasterSlaveConflict,  "HDX 9004");
   SetCompatibility(H323Connection::e_NoUserInputCapability,   "AltiServ-ITG");
-  AddCompatibility(H323Connection::e_H224MustBeSession3,      "HDX");
+  SetCompatibility(H323Connection::e_H224MustBeSession3,      "HDX");
+  SetCompatibility(H323Connection::e_NeedMSDAfterNonEmptyTCS, "Avaya|Radvision");
 
   PTRACE(4, "H323\tCreated endpoint.");
 }
