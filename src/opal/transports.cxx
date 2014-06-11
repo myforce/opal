f04e02fe14aee6dac32b021ed7ecde1b8c85f4be
Robert Jongbloed <robertj@voxlucida.com.au>
2014-06-11 08:49:40 +0000

    Fixed OpalTransport::GetInterface() for TLS and WebSocket.
    git-svn-id: svn://svn.code.sf.net/p/opalvoip/code/opal/branches/v3_14@32134 023b2edf-31b2-4de3-b41e-bca80c47788f

--- src/opal/transports.cxx
+++ src/opal/transports.cxx
@@ -1112,7 +1112,7 @@ OpalTransportIP::OpalTransportIP(OpalEndPoint & end,
 PString OpalTransportIP::GetInterface() const
 {
   if (IsOpen()) {
-    PIPSocket * socket = dynamic_cast<PIPSocket *>(m_channel);
+    PIPSocket * socket = dynamic_cast<PIPSocket *>(m_channel->GetBaseReadChannel());
     if (socket != NULL) {
       PIPSocket::Address ip;
       if (socket->PIPSocket::GetLocalAddress(ip))
