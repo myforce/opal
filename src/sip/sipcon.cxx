ad76c49ee061dccb71235e0e903e11463d941f4b
Robert Jongbloed <robertj@voxlucida.com.au>
2014-08-28 04:16:50 +0000

    Merged revision(s) 32609-32610 from opal/branches/v3_14: Fixed merge error with suppression of INFO message for video fast update if remote indicates an error.

--- src/sip/sipcon.cxx
+++ src/sip/sipcon.cxx
@@ -223,6 +223,9 @@ SIPConnection::SIPConnection(const Init & init)
   , m_resolveMultipleFormatReINVITE(true)
   , m_symmetricOpenStream(false)
   , m_appearanceCode(init.m_endpoint.GetDefaultAppearanceCode())
+#if OPAL_VIDEO
+  , m_canDoVideoFastUpdateINFO(true)
+#endif
   , m_sessionTimer(init.m_endpoint.GetThreadPool(), init.m_endpoint, init.m_token, &SIPConnection::OnSessionTimeout)
   , m_prackMode((PRACKMode)m_stringOptions.GetInteger(OPAL_OPT_PRACK_MODE, init.m_endpoint.GetDefaultPRACKMode()))
   , m_prackEnabled(false)
@@ -3290,7 +3293,7 @@ bool SIPConnection::OnMediaCommand(OpalMediaStream & stream, const OpalMediaComm
   bool done = OpalRTPConnection::OnMediaCommand(stream, command);
 
 #if OPAL_VIDEO
-  if (PIsDescendant(&command, OpalVideoUpdatePicture) && (stream.IsSource() == (&stream.GetConnection() == this))) {
+  if (m_canDoVideoFastUpdateINFO && PIsDescendant(&command, OpalVideoUpdatePicture) && (stream.IsSource() == (&stream.GetConnection() == this))) {
     if ((m_stringOptions.GetInteger(OPAL_OPT_VIDUP_METHODS, OPAL_OPT_VIDUP_METHOD_DEFAULT)&OPAL_OPT_VIDUP_METHOD_OOB) == 0) {
       PTRACE(5, "RTPCon\tINFO picture_fast_update disabled in string options");
     }
@@ -3307,6 +3310,8 @@ bool SIPConnection::OnMediaCommand(OpalMediaStream & stream, const OpalMediaComm
                              "</media_control>");
       if (SendINFO(params))
         done = true;
+      else
+        m_canDoVideoFastUpdateINFO = false;
     }
   }
 #endif
