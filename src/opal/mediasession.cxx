c41c6119a8425603d8040b30ce9f8e3be57d4350
Robert Jongbloed <robertj@voxlucida.com.au>
2014-11-14 07:32:54 +0000

    Fixed statistics text output for zero time.
    git-svn-id: svn://svn.code.sf.net/p/opalvoip/code/opal/trunk@33119 023b2edf-31b2-4de3-b41e-bca80c47788f

--- src/opal/mediasession.cxx
+++ src/opal/mediasession.cxx
@@ -247,7 +247,7 @@ static PString InternalGetRate(const PTime & lastUpdate,
   if (lastUpdate.IsValid() && previousUpdate.IsValid()) {
     PTimeInterval interval = lastUpdate - previousUpdate;
     if (interval == 0)
-      return '0' + units;
+      return PString('0') + units;
     return PString(PString::ScaleSI, (lastValue - previousValue)*1000.0/interval.GetMilliSeconds(), decimals) + units;
   }
 
