33b2c856399a03fff2b931e7671dc6b3ce90a4f3
Robert Jongbloed <robertj@voxlucida.com.au>
2015-04-08 00:03:21 +0000

    Tweak for handling SIP to H.323 gateway.
    git-svn-id: svn://svn.code.sf.net/p/opalvoip/code/opal/trunk@33489 023b2edf-31b2-4de3-b41e-bca80c47788f

--- src/h323/h323.cxx
+++ src/h323/h323.cxx
@@ -1408,7 +1408,9 @@ PBoolean H323Connection::OnReceivedSignalConnect(const H323SignalPDU & pdu)
   m_signallingChannel->SetReadTimeout(connectionState < EstablishedConnection ? MonitorCallStartTime : MonitorCallStatusTime);
 
   // Set connected phase now so logic for not sending media before connected is not triggered
-  InternalOnConnected();
+  PSafePtr<OpalConnection> otherParty = GetOtherPartyConnection();
+  if (otherParty != NULL && !otherParty->IsNetworkConnection())
+    InternalOnConnected();
 
   // Check for fastStart data and start fast
   if (connect.HasOptionalField(H225_Connect_UUIE::e_fastStart))
