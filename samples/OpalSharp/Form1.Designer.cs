d0160f71eb618dbda0f1f128c07de6a8647cceb9
Robert Jongbloed <robertj@voxlucida.com.au>
2015-06-14 17:15:06 +0000

    Added sample C# application.
    git-svn-id: svn://svn.code.sf.net/p/opalvoip/code/opal/trunk@33633 023b2edf-31b2-4de3-b41e-bca80c47788f

---
+++ samples/OpalSharp/Form1.Designer.cs
@@ -0,0 +1,230 @@
+﻿namespace OpalSharp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Initialise = new System.Windows.Forms.Button();
+            this.Shutdown = new System.Windows.Forms.Button();
+            this.Register = new System.Windows.Forms.Button();
+            this.host = new System.Windows.Forms.TextBox();
+            this.user = new System.Windows.Forms.TextBox();
+            this.password = new System.Windows.Forms.TextBox();
+            this.Call = new System.Windows.Forms.Button();
+            this.urlToCall = new System.Windows.Forms.TextBox();
+            this.LogLevel = new System.Windows.Forms.NumericUpDown();
+            this.Unregister = new System.Windows.Forms.Button();
+            this.HangUp = new System.Windows.Forms.Button();
+            this.LogFile = new System.Windows.Forms.TextBox();
+            this.StunServer = new System.Windows.Forms.TextBox();
+            this.Answer = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.LogLevel)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Initialise
+            // 
+            this.Initialise.Location = new System.Drawing.Point(30, 12);
+            this.Initialise.Name = "Initialise";
+            this.Initialise.Size = new System.Drawing.Size(75, 23);
+            this.Initialise.TabIndex = 0;
+            this.Initialise.Text = "Initialise";
+            this.Initialise.UseVisualStyleBackColor = true;
+            this.Initialise.Click += new System.EventHandler(this.Initialise_Click);
+            // 
+            // Shutdown
+            // 
+            this.Shutdown.Enabled = false;
+            this.Shutdown.Location = new System.Drawing.Point(30, 34);
+            this.Shutdown.Name = "Shutdown";
+            this.Shutdown.Size = new System.Drawing.Size(75, 23);
+            this.Shutdown.TabIndex = 1;
+            this.Shutdown.Text = "Shutdown";
+            this.Shutdown.UseVisualStyleBackColor = true;
+            this.Shutdown.Click += new System.EventHandler(this.Shutdown_Click);
+            // 
+            // Register
+            // 
+            this.Register.Enabled = false;
+            this.Register.Location = new System.Drawing.Point(30, 77);
+            this.Register.Name = "Register";
+            this.Register.Size = new System.Drawing.Size(75, 23);
+            this.Register.TabIndex = 2;
+            this.Register.Text = "Register";
+            this.Register.UseVisualStyleBackColor = true;
+            this.Register.Click += new System.EventHandler(this.Register_Click);
+            // 
+            // host
+            // 
+            this.host.Location = new System.Drawing.Point(128, 77);
+            this.host.Name = "host";
+            this.host.Size = new System.Drawing.Size(100, 20);
+            this.host.TabIndex = 3;
+            this.host.Text = "ekiga.net";
+            // 
+            // user
+            // 
+            this.user.Location = new System.Drawing.Point(234, 77);
+            this.user.Name = "user";
+            this.user.Size = new System.Drawing.Size(100, 20);
+            this.user.TabIndex = 4;
+            this.user.Text = "user";
+            // 
+            // password
+            // 
+            this.password.Location = new System.Drawing.Point(344, 77);
+            this.password.Name = "password";
+            this.password.Size = new System.Drawing.Size(100, 20);
+            this.password.TabIndex = 5;
+            this.password.UseSystemPasswordChar = true;
+            // 
+            // Call
+            // 
+            this.Call.Enabled = false;
+            this.Call.Location = new System.Drawing.Point(30, 143);
+            this.Call.Name = "Call";
+            this.Call.Size = new System.Drawing.Size(75, 23);
+            this.Call.TabIndex = 6;
+            this.Call.Text = "Call";
+            this.Call.UseVisualStyleBackColor = true;
+            this.Call.Click += new System.EventHandler(this.Call_Click);
+            // 
+            // urlToCall
+            // 
+            this.urlToCall.Location = new System.Drawing.Point(128, 143);
+            this.urlToCall.Name = "urlToCall";
+            this.urlToCall.Size = new System.Drawing.Size(316, 20);
+            this.urlToCall.TabIndex = 7;
+            this.urlToCall.Text = "sip:500@ekiga.net";
+            // 
+            // LogLevel
+            // 
+            this.LogLevel.Location = new System.Drawing.Point(128, 12);
+            this.LogLevel.Name = "LogLevel";
+            this.LogLevel.Size = new System.Drawing.Size(35, 20);
+            this.LogLevel.TabIndex = 8;
+            this.LogLevel.Value = new decimal(new int[] {
+            4,
+            0,
+            0,
+            0});
+            // 
+            // Unregister
+            // 
+            this.Unregister.Enabled = false;
+            this.Unregister.Location = new System.Drawing.Point(30, 103);
+            this.Unregister.Name = "Unregister";
+            this.Unregister.Size = new System.Drawing.Size(75, 23);
+            this.Unregister.TabIndex = 9;
+            this.Unregister.Text = "Unregister";
+            this.Unregister.UseVisualStyleBackColor = true;
+            this.Unregister.Click += new System.EventHandler(this.Unregister_Click);
+            // 
+            // HangUp
+            // 
+            this.HangUp.Enabled = false;
+            this.HangUp.Location = new System.Drawing.Point(30, 172);
+            this.HangUp.Name = "HangUp";
+            this.HangUp.Size = new System.Drawing.Size(75, 23);
+            this.HangUp.TabIndex = 10;
+            this.HangUp.Text = "Hang Up";
+            this.HangUp.UseVisualStyleBackColor = true;
+            this.HangUp.Click += new System.EventHandler(this.HangUp_Click);
+            // 
+            // LogFile
+            // 
+            this.LogFile.AllowDrop = true;
+            this.LogFile.Location = new System.Drawing.Point(169, 12);
+            this.LogFile.Name = "LogFile";
+            this.LogFile.Size = new System.Drawing.Size(275, 20);
+            this.LogFile.TabIndex = 11;
+            this.LogFile.Text = "C:\\work\\opal.log";
+            // 
+            // StunServer
+            // 
+            this.StunServer.Location = new System.Drawing.Point(128, 37);
+            this.StunServer.Name = "StunServer";
+            this.StunServer.Size = new System.Drawing.Size(131, 20);
+            this.StunServer.TabIndex = 12;
+            this.StunServer.Text = "stun.ekiga.net";
+            // 
+            // Answer
+            // 
+            this.Answer.Enabled = false;
+            this.Answer.Location = new System.Drawing.Point(128, 172);
+            this.Answer.Name = "Answer";
+            this.Answer.Size = new System.Drawing.Size(75, 23);
+            this.Answer.TabIndex = 13;
+            this.Answer.Text = "Answer";
+            this.Answer.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(476, 214);
+            this.Controls.Add(this.Answer);
+            this.Controls.Add(this.StunServer);
+            this.Controls.Add(this.LogFile);
+            this.Controls.Add(this.HangUp);
+            this.Controls.Add(this.Unregister);
+            this.Controls.Add(this.LogLevel);
+            this.Controls.Add(this.urlToCall);
+            this.Controls.Add(this.Call);
+            this.Controls.Add(this.password);
+            this.Controls.Add(this.user);
+            this.Controls.Add(this.host);
+            this.Controls.Add(this.Register);
+            this.Controls.Add(this.Shutdown);
+            this.Controls.Add(this.Initialise);
+            this.Name = "Form1";
+            this.Text = "OPAL";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.LogLevel)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Initialise;
+        private System.Windows.Forms.Button Shutdown;
+        private System.Windows.Forms.Button Register;
+        private System.Windows.Forms.TextBox host;
+        private System.Windows.Forms.TextBox user;
+        private System.Windows.Forms.TextBox password;
+        private System.Windows.Forms.Button Call;
+        private System.Windows.Forms.TextBox urlToCall;
+        private System.Windows.Forms.NumericUpDown LogLevel;
+        private System.Windows.Forms.Button Unregister;
+        private System.Windows.Forms.Button HangUp;
+        private System.Windows.Forms.TextBox LogFile;
+        private System.Windows.Forms.TextBox StunServer;
+        private System.Windows.Forms.Button Answer;
+    }
+}
+
---
+++ samples/OpalSharp/Form1.cs
@@ -0,0 +1,241 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace OpalSharp
+{
+    public partial class Form1 : Form
+    {
+        OpalContext m_opalContext;
+        System.Threading.Thread m_opalThread;
+        string m_opalCallToken;
+
+        delegate void EnableCallback(Control ctrl, bool enable);
+
+        private void EnableSafely(Control ctrl, bool enable)
+        {
+            if (ctrl.InvokeRequired)
+            {
+                EnableCallback d = new EnableCallback(EnableSafely);
+                this.Invoke(d, new object[] { ctrl, enable  });
+            }
+            else
+            {
+                ctrl.Enabled = enable;
+            }
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void HandleOpalMessages()
+        {
+            OpalMessagePtr msg = new OpalMessagePtr();
+            while (m_opalContext.GetMessage(msg, System.UInt32.MaxValue))
+            {
+                // Handle the message
+                switch (msg.GetType())
+                {
+                    case OpalMessageType.OpalIndRegistration:
+                        OpalStatusRegistration reg = msg.GetRegistrationStatus();
+                        switch (reg.status)
+                        {
+                            case OpalRegistrationStates.OpalRegisterSuccessful:
+                                MessageBox.Show("Registration complete to " + reg.serverName);
+                                EnableSafely(Unregister, true);
+                                break;
+                            case OpalRegistrationStates.OpalRegisterFailed:
+                                MessageBox.Show("Registration failed: " + reg.error);
+                                EnableSafely(Register, true);
+                                break;
+                        }
+                        break;
+
+                    case OpalMessageType.OpalIndIncomingCall :
+                        EnableSafely(Call, false);
+                        EnableSafely(Answer, true);
+                        EnableSafely(HangUp, true);
+                        break;
+
+                    case OpalMessageType.OpalIndCallCleared:
+                        EnableSafely(Call, true);
+                        EnableSafely(Answer, false);
+                        EnableSafely(HangUp, false);
+                        MessageBox.Show("Call ended: " + msg.GetCallCleared().reason);
+                        break;
+                }
+            }
+        }
+
+        private void Initialise_Click(object sender, EventArgs e)
+        {
+            if (m_opalContext == null)
+            {
+                Initialise.Enabled = false;
+                LogLevel.Enabled = false;
+                LogFile.Enabled = false;
+                StunServer.Enabled = false;
+
+                m_opalContext = new OpalContext();
+                if (m_opalContext.Initialise(OPAL.OPAL_PREFIX_SIP + " " + OPAL.OPAL_PREFIX_PCSS +
+                                             " --trace-level " + LogLevel.Value + " --output " + LogFile.Text) == 0)
+                    MessageBox.Show("Failed to initialise", "OPAL", MessageBoxButtons.OK);
+                else
+                {
+                    m_opalThread = new System.Threading.Thread(new System.Threading.ThreadStart(HandleOpalMessages));
+                    m_opalThread.Start();
+
+                    OpalMessagePtr msg = new OpalMessagePtr(OpalMessageType.OpalCmdSetGeneralParameters);
+                    OpalParamGeneral gen = msg.GetGeneralParams();
+                    gen.natMethod = "STUN";
+                    gen.natServer = StunServer.Text;
+                    gen.autoRxMedia = gen.autoTxMedia = "audio video";
+                    gen.mediaMask = "!G.711*\n!H.263*"; // Kind of backwards, it's a mask with negative entries, so just get G.711/H.263
+                    //gen.videoOutputDevice = "MSWIN STYLE=";
+                    OpalMessagePtr result = new OpalMessagePtr();
+                    if (!m_opalContext.SendMessage(msg, result))
+                        MessageBox.Show("Could not set general parameters: " + result.GetCommandError(), "OPAL", MessageBoxButtons.OK);
+                    else
+                    {
+                        msg = new OpalMessagePtr(OpalMessageType.OpalCmdSetProtocolParameters);
+                        OpalParamProtocol proto = msg.GetProtocolParams();
+                        proto.prefix = OPAL.OPAL_PREFIX_SIP;
+                        proto.interfaceAddresses = "0.0.0.0"; // All interfaces
+                        if (!m_opalContext.SendMessage(msg))
+                            MessageBox.Show("Could not set general parameters", "OPAL", MessageBoxButtons.OK);
+                        else
+                        {
+                            Shutdown.Enabled = true;
+                            Register.Enabled = true;
+                            Call.Enabled = true;
+                            return;
+                        }
+                    }
+                }
+
+                Initialise.Enabled = true;
+                LogLevel.Enabled = true;
+                LogFile.Enabled = true;
+                StunServer.Enabled = true;
+            }
+        }
+
+        private void Shutdown_Click(object sender, EventArgs e)
+        {
+            if (m_opalContext != null)
+            {
+                Shutdown.Enabled = false;
+                Register.Enabled = false;
+                Unregister.Enabled = false;
+                Call.Enabled = false;
+                HangUp.Enabled = false;
+
+                m_opalContext.ShutDown(); // This will make GetMessage() return false
+                m_opalThread.Join();
+                m_opalThread = null;
+                m_opalContext = null;
+
+                Initialise.Enabled = true;
+                LogLevel.Enabled = true;
+                LogFile.Enabled = true;
+                StunServer.Enabled = true;
+            }
+        }
+
+        private void Register_Click(object sender, EventArgs e)
+        {
+            if (m_opalContext != null)
+            {
+                Register.Enabled = false;
+                host.Enabled = false;
+                user.Enabled = false;
+                password.Enabled = false;
+
+                OpalMessagePtr msg = new OpalMessagePtr(OpalMessageType.OpalCmdRegistration);
+                OpalParamRegistration reg = msg.GetRegistrationParams();
+                reg.protocol = "sip";
+                reg.hostName = host.Text;
+                reg.identifier = user.Text;
+                reg.password = password.Text;
+                reg.timeToLive = 300;
+                //reg.attributes = "compatibility=RFC5626"
+                OpalMessagePtr result = new OpalMessagePtr();
+                if (m_opalContext.SendMessage(msg, result))
+                    return;
+
+                MessageBox.Show("Could not start registration: " + result.GetCommandError(), "OPAL", MessageBoxButtons.OK);
+                Register.Enabled = true;
+                host.Enabled = true;
+                user.Enabled = true;
+                password.Enabled = true;
+            }
+        }
+
+        private void Unregister_Click(object sender, EventArgs e)
+        {
+            if (m_opalContext != null)
+            {
+                OpalMessagePtr msg = new OpalMessagePtr(OpalMessageType.OpalCmdRegistration);
+                OpalParamRegistration reg = msg.GetRegistrationParams();
+                reg.protocol = "sip";
+                reg.hostName = host.Text;
+                reg.identifier = user.Text;
+                reg.password = password.Text;
+                reg.timeToLive = 300;
+                //reg.attributes = "compatibility=RFC5626"
+                OpalMessagePtr result = new OpalMessagePtr();
+                if (!m_opalContext.SendMessage(msg, result))
+                    MessageBox.Show("Could not set start registration: " + result.GetCommandError(), "OPAL", MessageBoxButtons.OK);
+                else
+                    Register.Enabled = false;
+            }
+        }
+
+        private void Call_Click(object sender, EventArgs e)
+        {
+            if (m_opalContext != null)
+            {
+                OpalMessagePtr msg = new OpalMessagePtr(OpalMessageType.OpalCmdSetUpCall);
+                OpalParamSetUpCall call = msg.GetCallSetUp();
+                call.partyB = urlToCall.Text;
+                OpalMessagePtr result = new OpalMessagePtr();
+                if (!m_opalContext.SendMessage(msg, result))
+                    MessageBox.Show("Could not start call: " + result.GetCommandError(), "OPAL", MessageBoxButtons.OK);
+                else
+                {
+                    m_opalCallToken = result.GetCallSetUp().callToken;
+                    Call.Enabled = false;
+                    HangUp.Enabled = true;
+                }
+            }
+        }
+
+        private void HangUp_Click(object sender, EventArgs e)
+        {
+            if (m_opalContext != null && m_opalCallToken.Length > 0)
+            {
+                HangUp.Enabled = false;
+
+                OpalMessagePtr msg = new OpalMessagePtr(OpalMessageType.OpalCmdClearCall);
+                OpalParamCallCleared call = msg.GetClearCall();
+                call.callToken = m_opalCallToken;
+                OpalMessagePtr result = new OpalMessagePtr();
+                if (!m_opalContext.SendMessage(msg, result))
+                    MessageBox.Show("Could not hang up call: " + result.GetCommandError(), "OPAL", MessageBoxButtons.OK);
+            }
+        }
+    }
+}
