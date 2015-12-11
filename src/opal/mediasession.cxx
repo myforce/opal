b8828a0d036b80261c4282fb104c7ee4780d0bee
Robert Jongbloed <robertj@voxlucida.com.au>
2015-12-11 20:04:51 +0000

    Fixed handling of empty INVITE, need to wait until have remote RTP address before allowing the RTP session to start.
    git-svn-id: svn://svn.code.sf.net/p/opalvoip/code/opal/branches/v3_16@34425 023b2edf-31b2-4de3-b41e-bca80c47788f

--- src/opal/mediasession.cxx
+++ src/opal/mediasession.cxx
@@ -508,7 +508,7 @@ ostream & operator<<(ostream & strm, OpalMediaTransportChannelTypes::SubChannels
 OpalMediaTransport::OpalMediaTransport(const PString & name)
   : m_name(name)
   , m_remoteBehindNAT(false)
-  , m_remoteAddressSet(true)
+  , m_remoteAddressSet(false)
   , m_packetSize(2048)
   , m_maxNoTransmitTime(0, 10)          // Sending data for 10 seconds, ICMP says still not there
   , m_started(false)
@@ -644,7 +644,6 @@ void OpalMediaTransport::RemoveReadNotifier(PObject * target, SubChannels subcha
 void OpalMediaTransport::SetRemoteBehindNAT()
 {
   m_remoteBehindNAT = true;
-  m_remoteAddressSet = !GetRemoteAddress().IsEmpty();
 }
 
 OpalMediaTransport::Transport::Transport(OpalMediaTransport * owner, SubChannels subchannel, PChannel * chan)
@@ -835,7 +834,11 @@ bool OpalTCPMediaTransport::SetRemoteAddress(const OpalTransportAddress & remote
 
   PTCPSocket & socket = dynamic_cast<PTCPSocket &>(*m_subchannels[0].m_channel);
   socket.SetPort(ap.GetPort());
-  return socket.Connect(ap.GetAddress());
+  if (!socket.Connect(ap.GetAddress()))
+    return false;
+
+  m_remoteAddressSet = true;
+  return true;
 }
 
 
@@ -1148,7 +1151,7 @@ bool OpalUDPMediaTransport::Write(const void * data, PINDEX length, SubChannels 
   if (writeSuccess)
     return true;
 
-  PTRACE(1, *this << "write (" << length << " bytes) error"
+  PTRACE(1, *this << "write to " << *dest << " (" << length << " bytes) error"
             " on " << subchannel << " subchannel to " << *dest <<
             " (" << socket->GetErrorNumber(PChannel::LastWriteError) << "):"
             " " << socket->GetErrorText(PChannel::LastWriteError));
